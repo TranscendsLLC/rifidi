b9c383050c2cea47a27abfc0eebf0957b1bd29f1
jerry <jerry@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2008-06-09 20:16:14 +0000

    Session Registory

---
+++ org.rifidi.edge.core/test/test/DummyConnectionInfo.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.rifidi.edge.core.readerAdapter.AbstractConnectionInfo;
+
+public class DummyConnectionInfo extends AbstractConnectionInfo{
+
+	@Override
+	public Class<?> getReaderAdapterType() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
