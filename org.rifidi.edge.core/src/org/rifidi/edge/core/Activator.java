862eac2d05b953cab67b66f5cd73dba595d71e31
jerry <jerry@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2008-06-09 20:15:45 +0000

    Session Registory

--- org.rifidi.edge.core/src/org/rifidi/edge/core/Activator.java
+++ org.rifidi.edge.core/src/org/rifidi/edge/core/Activator.java
@@ -2,15 +2,21 @@ package org.rifidi.edge.core;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.rifidi.edge.core.session.SessionRegistryService;
+import org.rifidi.edge.core.session.SessionRegistryServiceImpl;
 
 public class Activator implements BundleActivator {
 
+	SessionRegistryService sessionService;
+	
 	/*
 	 * (non-Javadoc)
 	 * 
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext context) throws Exception {
+		sessionService = new SessionRegistryServiceImpl();
+		context.registerService(SessionRegistryService.class.getName(), sessionService, null);
 	}
 
 	/*
