c6f2d36aef7a6507f53464928f0763703093661b
matt <matt@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2008-06-09 18:59:21 +0000

    Initial import.

---
+++ rifidi/trunk/org.rifidi.edge.adapter.alien/src/org/rifidi/edge/alien/adapter/AlienReaderAdapterFactory.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.rifidi.edge.alien.adapter;
+
+import org.rifidi.edge.core.readerAdapter.AbstractConnectionInfo;
+import org.rifidi.edge.core.readerAdapter.ISpecificReaderAdapterFactory;
+
+/**
+ * @author Matthew Dean - matt@pramari.com
+ *
+ */
+public class AlienReaderAdapterFactory implements ISpecificReaderAdapterFactory {
+
+	/* (non-Javadoc)
+	 * @see org.rifidi.edge.core.readerAdapter.ISpecificReaderAdapterFactory#createSpecReaderAdapter(org.rifidi.edge.core.readerAdapter.AbstractConnectionInfo)
+	 */
+	@Override
+	public void createSpecReaderAdapter(AbstractConnectionInfo pattern) {
+		
+	}
+
+}
