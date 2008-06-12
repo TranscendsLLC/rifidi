051f8c00dcdde8bb4ace4315d784b26e901def26
andreas <andreas@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2008-06-12 15:44:36 +0000

    Initial import.

---
+++ rifidi/trunk/org.rifidi.edge.log4j/src/org/rifidi/edge/log4j/Activator.java
@@ -0,0 +1,24 @@
+package org.rifidi.edge.log4j;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		System.out.println("Bundle " + this.getClass().getName() + " loaded");
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		System.out.println("Bundle " + this.getClass().getName() + " stopped");
+	}
+
+}
