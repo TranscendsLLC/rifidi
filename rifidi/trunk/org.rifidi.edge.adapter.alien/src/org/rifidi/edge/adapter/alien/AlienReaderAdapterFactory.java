482b79185e50aa014a773f33f2ffa659cd4c17fa
jerry <jerry@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2008-06-12 19:32:53 +0000

    System.out.println() changed to logger.debug()

--- rifidi/trunk/org.rifidi.edge.adapter.alien/src/org/rifidi/edge/adapter/alien/AlienReaderAdapterFactory.java
+++ rifidi/trunk/org.rifidi.edge.adapter.alien/src/org/rifidi/edge/adapter/alien/AlienReaderAdapterFactory.java
@@ -3,6 +3,8 @@
  */
 package org.rifidi.edge.adapter.alien;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.rifidi.edge.core.readerAdapter.AbstractConnectionInfo;
 import org.rifidi.edge.core.readerAdapter.IReaderAdapter;
 import org.rifidi.edge.core.readerAdapter.ISpecificReaderAdapterFactory;
@@ -12,14 +14,16 @@ import org.rifidi.edge.core.readerAdapter.ISpecificReaderAdapterFactory;
  *
  */
 public class AlienReaderAdapterFactory implements ISpecificReaderAdapterFactory {
-
+	private static final Log logger = LogFactory.getLog(AlienReaderAdapterFactory.class);	
 	/* (non-Javadoc)
 	 * @see org.rifidi.edge.core.readerAdapter.ISpecificReaderAdapterFactory#createSpecificReaderAdapter(org.rifidi.edge.core.readerAdapter.AbstractConnectionInfo)
 	 */
 	@Override
 	public IReaderAdapter createSpecificReaderAdapter(AbstractConnectionInfo abstractConnectionInfo) {
+		
 		if(abstractConnectionInfo instanceof AlienConnectionInfo)
 		{
+			logger.debug("createSpecificReaderAdapter called.");
 			return new AlienReaderAdapter((AlienConnectionInfo)abstractConnectionInfo);
 		}
 		return null;
