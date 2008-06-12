c7609cd99c59ef40255c1af07bcaf581ac7ec5c3
andreas <andreas@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2008-06-12 23:06:15 +0000

    Initial import.

---
+++ rifidi/trunk/org.rifidi.edge.services/src/org/rifidi/edge/services/Activator.java
@@ -0,0 +1,31 @@
+package org.rifidi.edge.services;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+
+public class Activator implements BundleActivator {
+
+	public static BundleContext context;
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		this.context = context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		context = null;
+	}
+
+	public static BundleContext getDefaultContext() {
+		return context;
+	}
+
+}
---
+++ rifidi/trunk/org.rifidi.edge.services/src/org/rifidi/services/annotations/Inject.java
@@ -0,0 +1,24 @@
+/*
+ *  Inject.java
+ *
+ *  Project:		RiFidi Designer - A Virtualization tool for 3D RFID environments
+ *  http://www.rifidi.org
+ *  http://rifidi.sourceforge.net
+ *  Copyright:	    Pramari LLC and the Rifidi Project
+ *  License:		Lesser GNU Public License (LGPL)
+ *  http://www.opensource.org/licenses/lgpl-license.html
+ */
+package org.rifidi.services.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * 
+ * 
+ * @author Jochen Mader - jochen@pramari.com - Mar 19, 2008
+ * 
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Inject {
+}
