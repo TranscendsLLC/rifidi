748a5c029615eb74291f2a9b5325e56d1ebf5e7f
jerry <jerry@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2008-06-13 17:46:53 +0000

    Exceptions added.

---
+++ org.rifidi.edge.core/src/org/rifidi/edge/core/exception/RifidiException.java
@@ -0,0 +1,29 @@
+package org.rifidi.edge.core.exception;
+
+public class RifidiException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6039685993579708974L;
+
+	public RifidiException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public RifidiException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RifidiException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RifidiException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ org.rifidi.edge.core/src/org/rifidi/edge/core/exception/adapter/RifidiAdapterIllegalStateException.java
@@ -0,0 +1,32 @@
+package org.rifidi.edge.core.exception.adapter;
+
+
+public class RifidiAdapterIllegalStateException extends
+		RifidiReaderAdapterException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5573663418847045444L;
+
+	public RifidiAdapterIllegalStateException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public RifidiAdapterIllegalStateException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RifidiAdapterIllegalStateException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public RifidiAdapterIllegalStateException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
