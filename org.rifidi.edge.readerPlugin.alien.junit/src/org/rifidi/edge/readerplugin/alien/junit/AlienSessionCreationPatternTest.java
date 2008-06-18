0b571e04f01d1d70910caa9422f32150b9b14b2f
matt <matt@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2008-06-18 17:32:09 +0000

    Initial import.

---
+++ org.rifidi.edge.readerPlugin.alien.junit/src/org/rifidi/edge/readerplugin/alien/junit/AlienSessionCreationPatternTest.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package org.rifidi.edge.readerplugin.alien.junit;
+
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Matthew Dean - matt@pramari.com
+ *
+ */
+public class AlienSessionCreationPatternTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	/**
+	 * Tests connecting the reader
+	 */
+	@Test
+	public void testIPAddress() {
+		Assert.fail();
+	}
+	
+	/**
+	 * Tests connecting the reader
+	 */
+	@Test
+	public void testPort() {
+		Assert.fail();
+	}
+
+}
