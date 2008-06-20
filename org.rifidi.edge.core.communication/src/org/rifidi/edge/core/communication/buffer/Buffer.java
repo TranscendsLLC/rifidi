6970ecebbb9f08edbeec5cfbf8945a86bc6a70b9
matt <matt@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2008-06-20 20:07:17 +0000

    Initial import.

---
+++ org.rifidi.edge.core.communication/src/org/rifidi/edge/core/communication/buffer/Buffer.java
@@ -0,0 +1,35 @@
+/* 
+ * Buffer.java
+ *  Created:	Jun 20, 2006
+ *  Project:	RiFidi Emulator - A Software Simulation Tool for RFID Devices
+ *  				http://www.rifidi.org
+ *  				http://rifidi.sourceforge.net
+ *  Copyright:	Pramari LLC and the Rifidi Project
+ *  License:	Lesser GNU Public License (LGPL)
+ *  				http://www.opensource.org/licenses/lgpl-license.html
+ */
+package org.rifidi.edge.core.communication.buffer;
+
+/**
+ * 
+ * 
+ * @author Matthew Dean - matt@pramari.com
+ */
+public class Buffer {
+	/**
+	 * 
+	 * @return
+	 */
+	public Object recieve() {
+		
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @param msg
+	 */
+	public void send(Object msg) {
+		
+	}
+}
