4f0a459c5e9c7bfd2c3bacb9ea69bbfe59b66c50
matt <matt@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2008-07-02 20:02:05 +0000

    Protocols fixed to reflect the changes in the Protocol interface.  They now add one byte at a time and return an object when they are finished.

--- org.rifidi.edge.readerPlugin.alien/src/org/rifidi/edge/readerPlugin/alien/AlienProtocol.java
+++ org.rifidi.edge.readerPlugin.alien/src/org/rifidi/edge/readerPlugin/alien/AlienProtocol.java
@@ -10,10 +10,8 @@
  */
 package org.rifidi.edge.readerPlugin.alien;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.rifidi.edge.core.communication.protocol.Protocol;
+import org.rifidi.edge.core.exception.readerConnection.RifidiInvalidMessageFormat;
 
 /**
  * Protocol class for the Alien reader.
@@ -22,39 +20,42 @@ import org.rifidi.edge.core.communication.protocol.Protocol;
  */
 public class AlienProtocol extends Protocol {
 
-	/*
-	 * (non-Javadoc)
+	private static final char TERMINATION_CHAR = '\0';
+
+	private StringBuilder buf;
+
+	/**
 	 * 
-	 * @see org.rifidi.edge.core.communication.buffer.Protocol#fromObject(java.lang.Object)
 	 */
-	@Override
-	public byte[] fromObject(Object arg) {
-		return ((String) arg).getBytes();
+	public AlienProtocol() {
+		buf = new StringBuilder();
 	}
-	
+
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see org.rifidi.edge.core.communication.buffer.Protocol#toObject(byte[])
+	 * @see org.rifidi.edge.core.communication.protocol.Protocol#add(byte)
 	 */
 	@Override
-	public List<Object> toObject(byte[] arg) {
-		List<Object> retVal = new ArrayList<Object>();
-
-		String input = new String(arg);
-
-		if (!input.equals("\0")) {
-			String[] splitstr = input.split("\0");
-
-			for (String s : splitstr) {
-				s = s.trim();
-				if (s.length() > 0) {
-					retVal.add(s);
-				}
-			}
+	public Object add(byte b) throws RifidiInvalidMessageFormat {
+		buf.append((char) b);
+		if ((char) b == AlienProtocol.TERMINATION_CHAR) {
+			String temp = buf.toString();
+			buf = new StringBuilder();
+			return temp;
 		}
 
-		return retVal;
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.rifidi.edge.core.communication.protocol.Protocol#toByteArray(java.lang.Object)
+	 */
+	@Override
+	public byte[] toByteArray(Object o) throws RifidiInvalidMessageFormat {
+		return ((String) o).getBytes();
 	}
 
 	// /*
@@ -82,6 +83,6 @@ public class AlienProtocol extends Protocol {
 	// retVal.add("\0");
 	// }
 	//
-	//		return retVal;
-	//	}
+	// return retVal;
+	// }
 }
