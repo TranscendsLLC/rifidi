080531b4efb5b5aa84b281bf46fe80a6743a2a18
matt <matt@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2008-07-02 20:02:07 +0000

    Protocols fixed to reflect the changes in the Protocol interface.  They now add one byte at a time and return an object when they are finished.

--- rifidi/trunk/org.rifidi.edge.readerPlugin.llrp/src/org/rifidi/edge/readerPlugin/llrp/LLRPProtocol.java
+++ rifidi/trunk/org.rifidi.edge.readerPlugin.llrp/src/org/rifidi/edge/readerPlugin/llrp/LLRPProtocol.java
@@ -10,9 +10,7 @@
  */
 package org.rifidi.edge.readerPlugin.llrp;
 
-import java.io.ByteArrayInputStream;
 import java.util.ArrayList;
-import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -21,26 +19,65 @@ import org.llrp.ltk.generated.messages.LLRPMessageFactory;
 import org.llrp.ltk.types.LLRPMessage;
 import org.rifidi.edge.common.utilities.converter.ByteAndHexConvertingUtility;
 import org.rifidi.edge.core.communication.protocol.Protocol;
+import org.rifidi.edge.core.exception.readerConnection.RifidiInvalidMessageFormat;
 
 /**
- * @author Matthew Dean - matt@pramari.com
  * 
+ * 
+ * @author Matthew Dean - matt@pramari.com
  */
 public class LLRPProtocol extends Protocol {
+
+	private ArrayList<Byte> byteArrayList;
+
+	private int msgSize;
 
 	/**
 	 * The log4j logger.
 	 */
 	private static final Log logger = LogFactory.getLog(LLRPProtocol.class);
 
+	public LLRPProtocol() {
+		byteArrayList = new ArrayList<Byte>();
+	}
+
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see org.rifidi.edge.core.communication.buffer.Protocol#fromObject(java.lang.Object)
+	 * @see org.rifidi.edge.core.communication.protocol.Protocol#add(byte)
 	 */
 	@Override
-	public byte[] fromObject(Object arg) {
-		LLRPMessage llrpm = (LLRPMessage) arg;
+	public Object add(byte b) throws RifidiInvalidMessageFormat {
+		byteArrayList.add(b);
+		LLRPMessage retVal = null;
+		if (byteArrayList.size() == 6) {
+			byte[] sizeArray = getByteArray(byteArrayList);
+			msgSize = calculateLLRPMessageLength(sizeArray);
+		} else if (byteArrayList.size() > 6) {
+			if (byteArrayList.size() == msgSize) {
+				byte[] finalArray = getByteArray(byteArrayList);
+				try {
+					retVal = LLRPMessageFactory.createLLRPMessage(finalArray);
+					byteArrayList.clear();
+					msgSize = 0;
+				} catch (InvalidLLRPMessageException e) {
+					logger.debug("Invalid LLRP message creation", e);
+					throw new RifidiInvalidMessageFormat();
+				}
+			}
+		}
+
+		return retVal;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.rifidi.edge.core.communication.protocol.Protocol#toByteArray(java.lang.Object)
+	 */
+	@Override
+	public byte[] toByteArray(Object o) throws RifidiInvalidMessageFormat {
+		LLRPMessage llrpm = (LLRPMessage) o;
 		try {
 			return llrpm.encodeBinary();
 		} catch (InvalidLLRPMessageException e) {
@@ -49,38 +86,52 @@ public class LLRPProtocol extends Protocol {
 		return null;
 	}
 
-	/*
-	 * (non-Javadoc)
+	// /*
+	// * (non-Javadoc)
+	// *
+	// * @see
+	// org.rifidi.edge.core.communication.buffer.Protocol#toObject(byte[])
+	// */
+	// @Override
+	// public List<Object> toObject(byte[] arg) {
+	//
+	// List<Object> retVal = new ArrayList<Object>();
+	// boolean more = true;
+	// while (more) {
+	// if (bais.available() > 0) {
+	// byte[] first = new byte[6];
+	// bais.read(first, 0, 6);
+	// int sizeOfMsg = calculateLLRPMessageLength(first);
+	// byte[] msg = new byte[sizeOfMsg];
+	// byte[] rest;
+	//
+	// rest = new byte[sizeOfMsg - 6];
+	// bais.read(rest, 0, sizeOfMsg - 6);
+	// System.arraycopy(first, 0, msg, 0, 6);
+	// System.arraycopy(rest, 0, msg, 6, rest.length);
+	//
+	// try {
+	// retVal.add(LLRPMessageFactory.createLLRPMessage(msg));
+	// } catch (InvalidLLRPMessageException e) {
+	// logger.debug("Invalid LLRP message creation", e);
+	// }
+	// } else {
+	// more = false;
+	// }
+	// }
+	//
+	// return retVal;
+	// }
+
+	/**
 	 * 
-	 * @see org.rifidi.edge.core.communication.buffer.Protocol#toObject(byte[])
 	 */
-	@Override
-	public List<Object> toObject(byte[] arg) {
-		ByteArrayInputStream bais = new ByteArrayInputStream(arg);
-
-		List<Object> retVal = new ArrayList<Object>();
-		boolean more = true;
-		while (more) {
-			if (bais.available() > 0) {
-				byte[] first = new byte[6];
-				bais.read(first, 0, 6);
-				int sizeOfMsg = calculateLLRPMessageLength(first);
-				byte[] msg = new byte[sizeOfMsg];
-				byte[] rest;
-
-				rest = new byte[sizeOfMsg - 6];
-				bais.read(rest, 0, sizeOfMsg - 6);
-				System.arraycopy(first, 0, msg, 0, 6);
-				System.arraycopy(rest, 0, msg, 6, rest.length);
-
-				try {
-					retVal.add(LLRPMessageFactory.createLLRPMessage(msg));
-				} catch (InvalidLLRPMessageException e) {
-					logger.debug("Invalid LLRP message creation", e);
-				}
-			} else {
-				more = false;
-			}
+	private static byte[] getByteArray(ArrayList<Byte> arg) {
+		byte[] retVal = new byte[arg.size()];
+		int omg = 0;
+		for (Byte b : arg) {
+			retVal[omg] = b.byteValue();
+			omg++;
 		}
 
 		return retVal;
