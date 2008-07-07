bd76fb3795dbb9a2ba25ec5977b5c4749c5d65fe
andreas <andreas@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2008-07-07 06:56:15 +0000

    Initial import.

---
+++ New Folder/src/org/rifidi/edge/readerplugin/dummy/DummyReaderPlugin.java
@@ -0,0 +1,34 @@
+package org.rifidi.edge.readerplugin.dummy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.rifidi.edge.core.readerplugin.ReaderPlugin;
+import org.rifidi.edge.core.readerplugin.commands.Command;
+import org.rifidi.edge.core.readerplugin.connectionmanager.ConnectionManager;
+import org.rifidi.edge.core.readerplugin.protocol.MessageProtocol;
+import org.rifidi.edge.readerplugin.dummy.commands.TagStreamCommand;
+import org.rifidi.edge.readerplugin.dummy.protocol.DummyMessageProtocol;
+
+public class DummyReaderPlugin implements ReaderPlugin {
+
+	@Override
+	public List<Class<? extends Command>> getAvailableCommands() {
+		ArrayList<Class<? extends Command>> commands = new ArrayList<Class<? extends Command>>();
+		commands.add(TagStreamCommand.class);
+		return commands;
+	}
+
+	@Override
+	public MessageProtocol getMessageProtocol() {
+		return new DummyMessageProtocol();
+	}
+
+	//FIXME implement ConnectionManager
+	@Override
+	public ConnectionManager getConnectionManager() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
