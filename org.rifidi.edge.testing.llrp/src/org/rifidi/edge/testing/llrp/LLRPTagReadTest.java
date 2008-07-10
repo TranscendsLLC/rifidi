6496702d6e2dbab59ec173b77ddd401b2342a977
jerry <jerry@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2008-07-10 20:04:48 +0000

    Initial import.

---
+++ org.rifidi.edge.testing.llrp/src/org/rifidi/edge/testing/llrp/LLRPTagReadTest.java
@@ -0,0 +1,17 @@
+package org.rifidi.edge.testing.llrp;
+
+
+import org.junit.After;
+import org.junit.Before;
+
+public class LLRPTagReadTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}
