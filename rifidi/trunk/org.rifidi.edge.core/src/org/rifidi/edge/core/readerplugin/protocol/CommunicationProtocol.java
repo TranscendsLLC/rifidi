0c7514b11671fd2339d76b55738d638818e3012d
kyle <kyle@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2008-07-10 19:00:27 +0000

    Added CommunicationProtocol

--- rifidi/trunk/org.rifidi.edge.core/src/org/rifidi/edge/core/readerplugin/protocol/CommunicationProtocol.java
+++ rifidi/trunk/org.rifidi.edge.core/src/org/rifidi/edge/core/readerplugin/protocol/CommunicationProtocol.java
@@ -1,9 +1,11 @@
 package org.rifidi.edge.core.readerplugin.protocol;
 
+import org.rifidi.edge.core.exceptions.RifidiInvalidMessageFormat;
+
 public interface CommunicationProtocol {
 	
 	// TODO figure out if you could send in whole Arrays
-	public Object byteToMessage(byte b);
+	public Object byteToMessage(byte b) throws RifidiInvalidMessageFormat;
 
-	public byte[] messageToByte(Object message);
+	public byte[] messageToByte(Object message) throws RifidiInvalidMessageFormat;
 }
