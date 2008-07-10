80860099ce94e01077206d649d06de816b2d4106
kyle <kyle@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2008-07-10 19:00:27 +0000

    Added CommunicationProtocol

---
+++ org.rifidi.edge.core/src/org/rifidi/edge/core/exceptions/RifidiInvalidMessageFormat.java
@@ -0,0 +1,17 @@
+package org.rifidi.edge.core.exceptions;
+
+/**
+ * This exception should be thrown by a Communication Protocol when the message
+ * that it is trying to convert bytes->object or object->bytes
+ * 
+ * @author Kyle Neumeier - kyle@pramari.com
+ * 
+ */
+public class RifidiInvalidMessageFormat extends RifidiException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8367649195678157160L;
+
+}
