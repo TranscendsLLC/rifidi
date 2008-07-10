4e540fd0a1745ea99014385bf6e0c65ea081ebcb
matt <matt@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2008-07-10 17:35:24 +0000

    Initial import.

---
+++ rifidi/trunk/org.rifidi.edge.testing.alien/src/org/rifidi/edge/testing/alien/AlienTestTagRead.java
@@ -0,0 +1,36 @@
+/* 
+ * AlienTestTagRead.java
+ *  Created:	Jul 10, 2008
+ *  Project:	RiFidi Emulator - A Software Simulation Tool for RFID Devices
+ *  				http://www.rifidi.org
+ *  				http://rifidi.sourceforge.net
+ *  Copyright:	Pramari LLC and the Rifidi Project
+ *  License:	Lesser GNU Public License (LGPL)
+ *  				http://www.opensource.org/licenses/lgpl-license.html
+ */
+package org.rifidi.edge.testing.alien;
+
+import org.junit.After;
+import org.junit.Before;
+
+/**
+ * @author Matthew Dean - matt@pramari.com
+ *
+ */
+public class AlienTestTagRead {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}
