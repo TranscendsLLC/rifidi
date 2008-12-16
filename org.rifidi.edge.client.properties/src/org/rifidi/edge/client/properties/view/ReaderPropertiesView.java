2b1e6f7100795901ee07633ebdaf04e6abdf34b2
tobias <tobias@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2008-12-16 10:21:35 +0000

    - bugfixes

--- org.rifidi.edge.client.properties/src/org/rifidi/edge/client/properties/view/ReaderPropertiesView.java
+++ org.rifidi.edge.client.properties/src/org/rifidi/edge/client/properties/view/ReaderPropertiesView.java
@@ -5,7 +5,8 @@ package org.rifidi.edge.client.properties.view;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.views.properties.PropertySheet;
 import org.rifidi.edge.client.connections.remotereader.RemoteReader;
@@ -15,11 +16,11 @@ import org.rifidi.edge.client.connections.views.EdgeServerConnectionView;
  * This is the properties view that displays properties of readers.
  * 
  * @author Kyle Neumeier - kyle@pramari.com
+ * @author Tobias Hoppenthaler - tobias@pramari.com
  * 
  */
 public class ReaderPropertiesView extends PropertySheet {
 
-	@SuppressWarnings("unused")
 	private Log logger = LogFactory.getLog(ReaderPropertiesView.class);
 
 	/**
@@ -28,10 +29,18 @@ public class ReaderPropertiesView extends PropertySheet {
 	 * another view
 	 */
 	public void partActivated(IWorkbenchPart part) {
-		ISelection sel = part.getSite().getSelectionProvider().getSelection();
-		
-		if (part instanceof EdgeServerConnectionView && sel instanceof RemoteReader) {
-			super.partActivated(part);
+		//TODO: improve
+		if (part instanceof EdgeServerConnectionView && part != null) {
+			IStructuredSelection sel = (TreeSelection) part.getSite()
+					.getSelectionProvider().getSelection();
+			try {
+				if (((RemoteReader) sel.getFirstElement()) instanceof RemoteReader) {
+					super.partActivated(part);
+				}
+			} catch (Exception e) {
+				logger.debug("ERROR: " + e.toString());
+			}
+
 		}
 	}
 }
