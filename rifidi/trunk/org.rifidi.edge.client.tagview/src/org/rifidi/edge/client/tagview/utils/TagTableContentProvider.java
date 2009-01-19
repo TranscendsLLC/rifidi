e745a90ee960a2223f5e03cf2ae22e904219a25a
tobias <tobias@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2009-01-19 12:23:29 +0000

    - first draft of ui

---
+++ rifidi/trunk/org.rifidi.edge.client.tagview/src/org/rifidi/edge/client/tagview/utils/TagTableContentProvider.java
@@ -0,0 +1,49 @@
+/* 
+ *  TagTableContentProvider.java
+ *  Created:	Jan 16, 2009
+ *  Project:	RiFidi org.rifidi.edge.client.tagview
+ *  				http://www.rifidi.org
+ *  				http://rifidi.sourceforge.net
+ *  Copyright:	Pramari LLC and the Rifidi Project
+ *  License:	Lesser GNU Public License (LGPL)
+ *  				http://www.opensource.org/licenses/lgpl-license.html
+ */
+package org.rifidi.edge.client.tagview.utils;
+
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.Viewer;
+
+/**
+ * @author Tobias Hoppenthaler - tobias@pramari.com
+ *
+ */
+public class TagTableContentProvider implements IStructuredContentProvider {
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.viewers.IStructuredContentProvider#getElements(java.lang.Object)
+	 */
+	@Override
+	public Object[] getElements(Object inputElement) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.viewers.IContentProvider#dispose()
+	 */
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.viewers.IContentProvider#inputChanged(org.eclipse.jface.viewers.Viewer, java.lang.Object, java.lang.Object)
+	 */
+	@Override
+	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
