0f1ffe2a0bc3e40de1b4416bad8c57e9137a70c1
tobias <tobias@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2009-02-16 12:27:52 +0000

    - name change

--- rifidi/trunk/org.rifidi.edge.client.ale/src/org/rifidi/edge/client/ale/logicalreader/wsdl/epcglobal/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "urn:epcglobal:alelr:wsdl:1")
-package org.rifidi.edge.client.ale.logicalreader.wsdl.epcglobal;
--- rifidi/trunk/org.rifidi.edge.client.ale/src/org/rifidi/edge/client/ale/xsd/epcglobal/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "urn:epcglobal:ale:xsd:1")
-package org.rifidi.edge.client.ale.xsd.epcglobal;
