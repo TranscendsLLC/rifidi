8b48a1c3f2be9debed3f544eb69290f1b18b25a8
jerry <jerry@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2009-04-11 00:32:49 +0000

    More thingmagic plugin stubs

---
+++ rifidi/trunk/org.rifidi.edge.readerplugin.thingmagic/src/org/rifidi/edge/readerplugin/thingmagic/command/ThingMagicGetTagListCommand.java
@@ -0,0 +1,18 @@
+package org.rifidi.edge.readerplugin.thingmagic.command;
+
+import org.rifidi.edge.readerplugin.thingmagic.ThingMagiv4_5Command;
+
+public class ThingMagicGetTagListCommand extends ThingMagiv4_5Command {
+
+	public ThingMagicGetTagListCommand(String commandID) {
+		super(commandID);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ rifidi/trunk/org.rifidi.edge.readerplugin.thingmagic/src/org/rifidi/edge/readerplugin/thingmagic/command/ThingMagicGetTagListCommandConfiguration.java
@@ -0,0 +1,26 @@
+package org.rifidi.edge.readerplugin.thingmagic.command;
+
+import org.rifidi.edge.core.commands.AbstractCommandConfiguration;
+
+public class ThingMagicGetTagListCommandConfiguration extends
+	AbstractCommandConfiguration<ThingMagicGetTagListCommand>{
+
+	@Override
+	public ThingMagicGetTagListCommand getCommand(String readerID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCommandDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getCommandName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
