9559955c09808fcc8d0a0600eae47bab7d087208
tobias <tobias@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2009-04-27 13:44:57 +0000

    - added comment - removed centering call

--- org.rifidi.edge.client.twodview/src/org/rifidi/edge/client/twodview/listeners/SiteViewMouseWheelListener.java
+++ org.rifidi.edge.client.twodview/src/org/rifidi/edge/client/twodview/listeners/SiteViewMouseWheelListener.java
@@ -26,6 +26,7 @@ import org.eclipse.swt.widgets.Listener;
 public class SiteViewMouseWheelListener implements Listener {
 
 	private ScalableLayeredPane sp = null;
+	@SuppressWarnings("unused")
 	private static final Log logger = LogFactory
 			.getLog(SiteViewMouseWheelListener.class);
 
@@ -37,36 +38,55 @@ public class SiteViewMouseWheelListener implements Listener {
 	@Override
 	public void handleEvent(Event event) {
 		zoom(event);
-		if (event.count > 0)
-			centerPane(event);
+		
+		
 	}
 
-	public void zoom(Event event) {
-		// WHEEL
+	private void zoom(Event event) {
+		/** if the event is caused by the wheel */
 		if (event.type == SWT.MouseWheel) {
+			/** if wheel is scrolled upwards and scale of the pane below 20 */
 			if (event.count > 0 && sp.getScale() < 20) {
+				/** scale up by 0.1 */
 				sp.setScale(sp.getScale() * 1.1);
 			}
+			/** if scrolled downwards and scale above 0.001 */
 			if (event.count < 0 // not = 0
 					&& sp.getScale() > 0.001) {
+				/** scale down by 0.1 */
 				sp.setScale(sp.getScale() / 1.1);
 
 			}
 		}
 	}
 
-	public void centerPane(Event event) {
-		// TODO COMMENT
-		int diffX = (sp.getBounds().width / 2) - event.x;
-		int diffY = (sp.getBounds().height / 2) - event.y;
+	public void centerPane(Event event){
+		centerPane(event.x,event.y);
+	}
+	
+	public void centerPane(int x, int y) {
+		/** horizontal difference of the x value to the middle of the pane */
+		int diffX = (sp.getBounds().width / 2) - x;
+		/** vertical difference of the y value to the middle of the pane */
+		int diffY = (sp.getBounds().height / 2) - y;
+		/** walk through all layers of the pane */
 		for (Object object : sp.getChildren()) {
+			/** walk through all the figures, placed on the layers
+			 *  and add the difference to center on the given x/y coordinates.
+			 */
 			for (Object obj : ((IFigure) object).getChildren()) {
+				/** cast the resulting object to an IFigure */
 				IFigure ifig = (IFigure) obj;
+				/** getting the bounds of the figure */
 				Rectangle bounds = ifig.getBounds();
+				/** move this figure horizontally */
 				bounds.x += diffX;
+				/** move this figure vertically */
 				bounds.y += diffY;
+				/** set the changes */
 				ifig.setBounds(bounds);
 			}
+			/** repaint the layers to see the effect of the changes */
 			((IFigure) object).repaint();
 		}
 		// logger.debug(sp.getBounds());
