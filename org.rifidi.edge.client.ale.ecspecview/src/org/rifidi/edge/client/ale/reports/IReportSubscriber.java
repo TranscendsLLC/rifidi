c905c06a952553dc4637b93c7c97b93e36470fbf
tobias <tobias@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2009-06-10 14:27:11 +0000

    - added comments

--- org.rifidi.edge.client.ale.ecspecview/src/org/rifidi/edge/client/ale/reports/IReportSubscriber.java
+++ org.rifidi.edge.client.ale.ecspecview/src/org/rifidi/edge/client/ale/reports/IReportSubscriber.java
@@ -13,12 +13,22 @@ package org.rifidi.edge.client.ale.reports;
 import org.rifidi.edge.client.ale.api.xsd.ale.epcglobal.ECReports;
 
 /**
+ * This interface should be implemented in order to receive reports from
+ * ReportReceiverSingleton. The implementing Class should then be subscribed to
+ * the latter.
  * 
  * @author Tobias Hoppenthaler - tobias@pramari.com
  * 
  */
 public interface IReportSubscriber {
 
+	/**
+	 * The reports get delivered by the ReportReceiverSingleton through this
+	 * method.
+	 * 
+	 * @param reports
+	 *            - reports to be delivered.
+	 */
 	public void pushReport(ECReports reports);
 
 }
