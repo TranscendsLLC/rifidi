a7ac400a63065fa3e10916d8c3c8b99791ac8501
matt <matt@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2009-09-08 22:46:42 +0000

    Massive header change for the edge server API only.

--- rifidi/trunk/org.rifidi.edge.api/src/org/rifidi/edge/api/jms/notifications/SessionRemovedNotification.java
+++ rifidi/trunk/org.rifidi.edge.api/src/org/rifidi/edge/api/jms/notifications/SessionRemovedNotification.java
@@ -1,3 +1,14 @@
+/*
+ * SessionRemovedNotification.java
+ * 
+ * Created:     July 22nd, 2009
+ * Project:       Rifidi Edge Server - A middleware platform for RFID applications
+ *                    http://www.rifidi.org
+ *                    http://rifidi.sourceforge.net
+ * Copyright:   Pramari LLC and the Rifidi Project
+ * License:     The software in this package is published under the terms of the EPL License
+ *                   A copy of the license is included in this distribution under Rifidi-License.txt 
+ */
 package org.rifidi.edge.api.jms.notifications;
 
 import java.io.Serializable;
