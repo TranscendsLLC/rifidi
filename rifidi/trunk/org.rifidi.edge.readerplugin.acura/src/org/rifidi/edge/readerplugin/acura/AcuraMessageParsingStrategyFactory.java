f50b93318f2864ab79a8733e8fc16a16a4e40cb5
matt <matt@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2009-12-08 22:16:05 +0000

    Message parsing strategy is now mostly done.

---
+++ rifidi/trunk/org.rifidi.edge.readerplugin.acura/src/org/rifidi/edge/readerplugin/acura/AcuraMessageParsingStrategyFactory.java
@@ -0,0 +1,31 @@
+/*
+ *  AcuraMessageParsingStrategyFactory.java
+ *
+ *  Created:	Dec 8, 2009
+ *  Project:	Rifidi Edge Server - A middleware platform for RFID applications
+ *  				http://www.rifidi.org
+ *  				http://rifidi.sourceforge.net
+ *  Copyright:	Pramari LLC and the Rifidi Project
+ *  License:	GNU Public License (GPL)
+ *  				http://www.opensource.org/licenses/gpl-3.0.html
+ */
+package org.rifidi.edge.readerplugin.acura;
+
+import org.rifidi.edge.core.sensors.sessions.MessageParsingStrategy;
+import org.rifidi.edge.core.sensors.sessions.MessageParsingStrategyFactory;
+
+/**
+ * @author Matthew Dean
+ *
+ */
+public class AcuraMessageParsingStrategyFactory implements
+		MessageParsingStrategyFactory {
+
+	/* (non-Javadoc)
+	 * @see org.rifidi.edge.core.sensors.sessions.MessageParsingStrategyFactory#createMessageParser()
+	 */
+	@Override
+	public MessageParsingStrategy createMessageParser() {
+		return null;
+	}
+}
