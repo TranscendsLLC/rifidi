fb19657c14639af909785e709d1b48995aa315dd
matt <matt@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2010-01-08 23:09:58 +0000

    Fixed the ThingMagic so that the protocol and timeout values are now adjustable.  Unfortunately this isn't currently testable, because we don't have a thingmagic.  I eyeballed the commands and they appear to be correct.  If there is a problem, I'll fix it.  -Matt

--- rifidi/trunk/org.rifidi.edge.readerplugin.thingmagic.commands/src/org/rifidi/edge/readerplugin/thingmagic/commands/ThingmagicGetTagListCommand.java
+++ rifidi/trunk/org.rifidi.edge.readerplugin.thingmagic.commands/src/org/rifidi/edge/readerplugin/thingmagic/commands/ThingmagicGetTagListCommand.java
@@ -27,9 +27,12 @@ import org.rifidi.edge.core.services.notification.data.ReadCycleMessageCreator;
 import org.rifidi.edge.core.services.notification.data.TagReadEvent;
 import org.rifidi.edge.readerplugin.thingmagic.AbstractThingmagicCommand;
 import org.rifidi.edge.readerplugin.thingmagic.ThingmagicReaderSession;
+import org.rifidi.edge.readerplugin.thingmagic.commands.helper.ThingMagicStringBuilder;
 
 /**
- * 
+ * The command for the thingmagic which will return a list of tags. Currently
+ * you can adjust the protocol and the timeout, although more changes may
+ * follow.
  * 
  * @author Matthew Dean
  */
@@ -39,6 +42,63 @@ public class ThingmagicGetTagListCommand extends AbstractThingmagicCommand {
 	private AtomicReference<String> reader = new AtomicReference<String>();
 	/** The logger for this class */
 	private Log logger = LogFactory.getLog(ThingmagicGetTagListCommand.class);
+
+	/** The timeout for the tag list command */
+	private Integer timeout = 1000;
+
+	/** Will it read class 0 tags? */
+	private boolean EPC0 = true;
+	/** Will it read class 1 tags? */
+	private boolean EPC1 = false;
+	/** Will it read Gen2 tags? */
+	private boolean GEN2 = false;
+	/**  */
+	private boolean ISO180006B = false;
+
+	/**
+	 * Sets the timeout for this command
+	 * 
+	 * @param timeout
+	 */
+	public void setTimeout(Integer timeout) {
+		this.timeout = timeout;
+	}
+
+	/**
+	 * Sets whether the reader will look for class 0 tags
+	 * 
+	 * @param ePC0
+	 */
+	public void setEPC0(boolean ePC0) {
+		EPC0 = ePC0;
+	}
+
+	/**
+	 * Sets whether the reader will look for class 1 tags
+	 * 
+	 * @param ePC1
+	 */
+	public void setEPC1(boolean ePC1) {
+		EPC1 = ePC1;
+	}
+
+	/**
+	 * Sets whether the reader will look for gen2 tags.
+	 * 
+	 * @param gEN2
+	 */
+	public void setGEN2(boolean gEN2) {
+		GEN2 = gEN2;
+	}
+
+	/**
+	 * Sets whether the reader will look for ISO-8600-6B tags.
+	 * 
+	 * @param iSO180006B
+	 */
+	public void setISO180006B(boolean iSO180006B) {
+		ISO180006B = iSO180006B;
+	}
 
 	/**
 	 * 
@@ -68,9 +128,11 @@ public class ThingmagicGetTagListCommand extends AbstractThingmagicCommand {
 
 		// Output will look like this:
 		// Ox303512B1F1FF795511B64E38|1
-		String message = "SELECT id, antenna_id FROM tag_id WHERE " +
-				"protocol_id='EPC1' " +
-				"set time_out=250;\r\n";
+		// String message = "SELECT id, antenna_id FROM tag_id WHERE "
+		// + "protocol_id='EPC1' " + "set time_out=250;\r\n";
+
+		String message = ThingMagicStringBuilder.getCommandString(timeout,
+				EPC0, EPC1, GEN2, ISO180006B);
 
 		try {
 			((ThingmagicReaderSession) this.sensorSession)
@@ -86,8 +148,9 @@ public class ThingmagicGetTagListCommand extends AbstractThingmagicCommand {
 		try {
 			incomingMessage = ((ThingmagicReaderSession) this.sensorSession)
 					.receiveMessage();
-			if(logger.isDebugEnabled()){
-				logger.debug("raw msg as string: " + new String(incomingMessage.message));
+			if (logger.isDebugEnabled()) {
+				logger.debug("raw msg as string: "
+						+ new String(incomingMessage.message));
 			}
 			String incoming = new String(incomingMessage.message).trim();
 			String[] tagString = incoming.split(splitregex);
--- rifidi/trunk/org.rifidi.edge.readerplugin.thingmagic.commands/src/org/rifidi/edge/readerplugin/thingmagic/commands/ThingmagicGetTagListCommandConfiguration.java
+++ rifidi/trunk/org.rifidi.edge.readerplugin.thingmagic.commands/src/org/rifidi/edge/readerplugin/thingmagic/commands/ThingmagicGetTagListCommandConfiguration.java
@@ -14,6 +14,8 @@ package org.rifidi.edge.readerplugin.thingmagic.commands;
 import javax.management.MBeanInfo;
 
 import org.rifidi.edge.core.configuration.annotations.JMXMBean;
+import org.rifidi.edge.core.configuration.annotations.Property;
+import org.rifidi.edge.core.configuration.annotations.PropertyType;
 import org.rifidi.edge.core.configuration.mbeanstrategies.AnnotationMBeanInfoStrategy;
 import org.rifidi.edge.core.sensors.commands.AbstractCommandConfiguration;
 
@@ -25,7 +27,7 @@ import org.rifidi.edge.core.sensors.commands.AbstractCommandConfiguration;
 @JMXMBean
 public class ThingmagicGetTagListCommandConfiguration extends
 		AbstractCommandConfiguration<ThingmagicGetTagListCommand> {
-	
+
 	/** Description of the command. */
 	public static final MBeanInfo mbeaninfo;
 	static {
@@ -33,25 +35,158 @@ public class ThingmagicGetTagListCommandConfiguration extends
 		mbeaninfo = strategy
 				.getMBeanInfo(ThingmagicGetTagListCommandConfiguration.class);
 	}
+	private Integer timeout = 1000;
+
+	private Integer epc0 = 0;
+	private Integer epc1 = 0;
+	private Integer gen2 = 1;
+	private Integer iso860006b = 0;
 
 	/*
 	 * (non-Javadoc)
-	 * @see org.rifidi.edge.core.sensors.commands.AbstractCommandConfiguration#getCommand(java.lang.String)
+	 * 
+	 * @see
+	 * org.rifidi.edge.core.sensors.commands.AbstractCommandConfiguration#getCommand
+	 * (java.lang.String)
 	 */
 	@Override
 	public ThingmagicGetTagListCommand getCommand(String readerID) {
-		ThingmagicGetTagListCommand c = new ThingmagicGetTagListCommand(super.getID());
+		ThingmagicGetTagListCommand c = new ThingmagicGetTagListCommand(super
+				.getID());
 		c.setReader(readerID);
+		c.setEPC0(this.intToBoolean(epc0));
+		c.setEPC1(this.intToBoolean(epc1));
+		c.setGEN2(this.intToBoolean(gen2));
+		c.setISO180006B(this.intToBoolean(iso860006b));
 		return c;
 	}
 
 	/*
 	 * (non-Javadoc)
+	 * 
 	 * @see org.rifidi.edge.core.configuration.RifidiService#getMBeanInfo()
 	 */
 	@Override
 	public MBeanInfo getMBeanInfo() {
 		return (MBeanInfo) mbeaninfo.clone();
+	}
+
+	/**
+	 * Gets the tag type.
+	 * 
+	 * @return the tagType
+	 */
+	@Property(displayName = "Timeout", description = "Sets the amount of time that must pass in milliseconds"
+			+ " before the reader stops looking for tags.  If this value is too small, the reader will not "
+			+ "read from all of its antennas.  Because of this, it is recommended that the value be at least 500ms.  "
+			+ "", writable = true, type = PropertyType.PT_INTEGER, minValue = ""
+			+ "500", maxValue = "16535", defaultValue = "1000")
+	public Integer getTimeout() {
+		return timeout;
+	}
+
+	/**
+	 * Sets the tag type.
+	 * 
+	 * @param tagType
+	 *            the tagType to set
+	 */
+	public void setTimeout(Integer timeout) {
+		this.timeout = timeout;
+	}
+
+	/**
+	 * @return the epc0
+	 */
+	@Property(displayName = "EPC0", description = "Sets whether the reader will look for Class 0 tags.  "
+			+ "Setting the value to 0 means the reader will not look for class 0 tags, setting the reader"
+			+ " to 1 will means the reader will look for class 0 tags.  If all the tag types are set to 0,"
+			+ " the function is ignored and all tag types will be searched for."
+			+ "", writable = true, type = PropertyType.PT_INTEGER, minValue = ""
+			+ "0", maxValue = "1", defaultValue = "0")
+	public Integer getEpc0() {
+		return epc0;
+	}
+
+	/**
+	 * @param epc0
+	 *            the epc0 to set
+	 */
+	public void setEpc0(Integer epc0) {
+		this.epc0 = epc0;
+	}
+
+	/**
+	 * @return the epc1
+	 */
+	@Property(displayName = "EPC1", description = "Sets whether the reader will look for Class 1 tags.  "
+			+ "Setting the value to 0 means the reader will not look for class 0 tags, setting the reader"
+			+ " to 1 will means the reader will look for class 0 tags.  If all the tag types are set to 0,"
+			+ " the function is ignored and all tag types will be searched for."
+			+ "", writable = true, type = PropertyType.PT_INTEGER, minValue = ""
+			+ "0", maxValue = "1", defaultValue = "0")
+	public Integer getEpc1() {
+		return epc1;
+	}
+
+	/**
+	 * @param epc1
+	 *            the epc1 to set
+	 */
+	public void setEpc1(Integer epc1) {
+		this.epc1 = epc1;
+	}
+
+	/**
+	 * @return the gen2
+	 */
+	@Property(displayName = "GEN2", description = "Sets whether the reader will look for Gen 2 tags.  "
+			+ "Setting the value to 0 means the reader will not look for class 0 tags, setting the reader"
+			+ " to 1 will means the reader will look for class 0 tags.  If all the tag types are set to 0,"
+			+ " the function is ignored and all tag types will be searched for."
+			+ "", writable = true, type = PropertyType.PT_INTEGER, minValue = ""
+			+ "0", maxValue = "1", defaultValue = "1")
+	public Integer getGen2() {
+		return gen2;
+	}
+
+	/**
+	 * @param gen2
+	 *            the gen2 to set
+	 */
+	public void setGen2(Integer gen2) {
+		this.gen2 = gen2;
+	}
+
+	/**
+	 * @return the iso860006b
+	 */
+	@Property(displayName = "ISO860006B", description = "Sets whether the reader will look for ISO860006b tags.  "
+			+ "Setting the value to 0 means the reader will not look for class 0 tags, setting the reader"
+			+ " to 1 will means the reader will look for class 0 tags.  If all the tag types are set to 0,"
+			+ " the function is ignored and all tag types will be searched for."
+			+ "", writable = true, type = PropertyType.PT_INTEGER, minValue = ""
+			+ "0", maxValue = "1", defaultValue = "0")
+	public Integer getIso860006b() {
+		return iso860006b;
+	}
+
+	/**
+	 * @param iso860006b
+	 *            the iso860006b to set
+	 */
+	public void setIso860006b(Integer iso860006b) {
+		this.iso860006b = iso860006b;
+	}
+
+	/*
+	 * Helper method that will convert an integer to a boolean.
+	 */
+	private boolean intToBoolean(int arg) {
+		if (arg == 0) {
+			return false;
+		}
+		return true;
 	}
 
 }
