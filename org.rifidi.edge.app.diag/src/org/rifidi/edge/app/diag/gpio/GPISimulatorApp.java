bbe6ab52d26bb62c311cf8f90025533aafbbcd57
matt <matt@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2010-03-12 20:31:56 +0000

    Added GPI simulation to the diagnostic application.  Next step: make it generic.

---
+++ org.rifidi.edge.app.diag/src/org/rifidi/edge/app/diag/gpio/GPISimulatorApp.java
@@ -0,0 +1,191 @@
+/*
+ *  GPISimulatorApp.java
+ *
+ *  Created:	Mar 10, 2010
+ *  Project:	Rifidi Edge Server - A middleware platform for RFID applications
+ *  				http://www.rifidi.org
+ *  				http://rifidi.sourceforge.net
+ *  Copyright:	Pramari LLC and the Rifidi Project
+ *  License:	GNU Public License (GPL)
+ *  				http://www.opensource.org/licenses/gpl-3.0.html
+ */
+package org.rifidi.edge.app.diag.gpio;
+
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.rifidi.edge.core.services.esper.EsperManagementService;
+import org.rifidi.edge.core.services.notification.data.gpio.GPIEvent;
+import org.rifidi.edge.core.services.notification.data.gpio.GPOEvent;
+
+import com.espertech.esper.client.EPServiceProvider;
+import com.espertech.esper.client.EPStatement;
+import com.espertech.esper.client.EventBean;
+import com.espertech.esper.client.StatementAwareUpdateListener;
+
+/**
+ * 
+ * 
+ * @author Matthew Dean
+ */
+public class GPISimulatorApp {
+
+	private static final Log logger = LogFactory.getLog(GPISimulatorApp.class);
+
+	/** Esper service */
+	protected volatile EsperManagementService esperService;
+
+	protected final Set<EPStatement> statements = new CopyOnWriteArraySet<EPStatement>();
+
+	/**
+	 */
+	public void start() {
+		System.out.println("start method called");
+		EPStatement query = esperService.getProvider().getEPAdministrator()
+				.createEPL("select * from GPIEvent");
+		statements.add(query);
+		StatementAwareUpdateListener stateUpdateListener = getSessionStateUpdateListener();
+		query.addListener(stateUpdateListener);
+	}
+
+	public StatementAwareUpdateListener getSessionStateUpdateListener() {
+		return new StatementAwareUpdateListener() {
+
+			/*
+			 * (non-Javadoc)
+			 * 
+			 * @see
+			 * com.espertech.esper.client.StatementAwareUpdateListener#update
+			 * (com.espertech.esper.client.EventBean[],
+			 * com.espertech.esper.client.EventBean[],
+			 * com.espertech.esper.client.EPStatement,
+			 * com.espertech.esper.client.EPServiceProvider)
+			 */
+			@Override
+			public void update(EventBean[] arg0, EventBean[] arg1,
+					EPStatement arg2, EPServiceProvider arg3) {
+				if (arg0 != null) {
+					for (EventBean eb : arg0) {
+						if (eb.getUnderlying() instanceof GPIEvent) {
+							logger.debug(eb.getUnderlying());
+							System.out.println(eb.getUnderlying());
+						}
+						if (eb.getUnderlying() instanceof GPOEvent) {
+							logger.debug(eb.getUnderlying());
+							System.out.println(eb.getUnderlying());
+						}
+					}
+				}
+			}
+		};
+	}
+
+	/**
+	 */
+	public void stop() {
+		for (EPStatement statement : statements) {
+			statement.destroy();
+		}
+	}
+
+	/*
+	 * Set the given GPO ports high.
+	 */
+	public void simGPIHigh(String readerID, Set<Integer> ports) {
+		for (Integer port : ports) {
+			GPIEvent gpi = new GPIEvent(readerID, port, true);
+			esperService.getProvider().getEPRuntime().sendEvent(gpi);
+		}
+	}
+
+	/*
+	 * Set the given GPO ports low.
+	 */
+	public void simGPILow(String readerID, Set<Integer> ports) {
+		for (Integer port : ports) {
+			GPIEvent gpi = new GPIEvent(readerID, port, false);
+			esperService.getProvider().getEPRuntime().sendEvent(gpi);
+		}
+	}
+
+	/*
+	 * Flash the given GPO ports high for the given number of seconds.
+	 */
+	public void simGPIFlashHigh(String readerID, Integer seconds,
+			Set<Integer> ports) {
+		new GPIFlash(readerID, ports, seconds, true).start();
+	}
+
+	/*
+	 * Flash the given GPO ports low for the given number of seconds.
+	 */
+	public void simGPIFlashLow(String readerID, Integer seconds,
+			Set<Integer> ports) {
+		new GPIFlash(readerID, ports, seconds, false).start();
+	}
+
+	/**
+	 * Called by spring
+	 * 
+	 * @param esperService
+	 *            the esperService to set
+	 */
+	public void setEsperService(EsperManagementService esperService) {
+		System.out.println("Setting esper");
+		this.esperService = esperService;
+	}
+
+
+	/**
+	 * A threaded class that can handle flashing the GPIs for a few seconds
+	 * without delaying output.
+	 * 
+	 * @author Matthew Dean
+	 */
+	private class GPIFlash extends Thread {
+
+		private String readerID;
+
+		private Set<Integer> ports;
+
+		private Integer seconds;
+
+		private boolean highfirst;
+
+		/**
+		 * 
+		 */
+		public GPIFlash(String readerID, Set<Integer> ports, Integer seconds,
+				boolean highfirst) {
+			this.readerID = readerID;
+			this.ports = ports;
+			this.seconds = seconds;
+			this.highfirst = highfirst;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see java.lang.Thread#run()
+		 */
+		@Override
+		public void run() {
+			if (highfirst) {
+				simGPIHigh(readerID, ports);
+			} else {
+				simGPILow(readerID, ports);
+			}
+			try {
+				Thread.sleep(seconds * 1000);
+			} catch (InterruptedException e) {
+			}
+			if (highfirst) {
+				simGPILow(readerID, ports);
+			} else {
+				simGPIHigh(readerID, ports);
+			}
+		}
+	}
+}
---
+++ org.rifidi.edge.app.diag/src/org/rifidi/edge/app/diag/gpio/GPISimulatorAppCommandProvider.java
@@ -0,0 +1,224 @@
+/*
+ *  GPISimulatorAppCommandProvider.java
+ *
+ *  Created:	Mar 12, 2010
+ *  Project:	Rifidi Edge Server - A middleware platform for RFID applications
+ *  				http://www.rifidi.org
+ *  				http://rifidi.sourceforge.net
+ *  Copyright:	Pramari LLC and the Rifidi Project
+ *  License:	GNU Public License (GPL)
+ *  				http://www.opensource.org/licenses/gpl-3.0.html
+ */
+package org.rifidi.edge.app.diag.gpio;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.eclipse.osgi.framework.console.CommandInterpreter;
+import org.eclipse.osgi.framework.console.CommandProvider;
+
+/**
+ * @author Matthew Dean
+ * 
+ */
+public class GPISimulatorAppCommandProvider implements CommandProvider {
+
+	private volatile GPISimulatorApp simApp;
+
+	/**
+	 * @return the simApp
+	 */
+	public GPISimulatorApp getSimApp() {
+		return simApp;
+	}
+
+	/**
+	 * @param simApp the simApp to set
+	 */
+	public void setSimApp(GPISimulatorApp simApp) {
+		this.simApp = simApp;
+	}
+
+	/**
+	 * Command for changing the given GPIs to high.
+	 * 
+	 * @param intp
+	 * @return
+	 */
+	public Object _simGPIHigh(CommandInterpreter intp) {
+		String readerArg = intp.nextArgument();
+		if (readerArg == null || readerArg.equals("")) {
+			intp.println("Usage: gpiHigh <readerID> <port1> "
+					+ "[port2] [port3] ...");
+			return null;
+		}
+
+		Set<Integer> ports = new HashSet<Integer>();
+		boolean done = false;
+		while (!done) {
+			String nextArg = intp.nextArgument();
+			if (nextArg != null && !nextArg.equals("")) {
+
+				Integer port = Integer.parseInt(nextArg);
+				ports.add(port);
+			} else {
+				done = true;
+			}
+		}
+		if (ports.isEmpty()) {
+			intp.println("Usage: gpiHigh <readerID> <port1> "
+					+ "[port2] [port3] ...");
+			return null;
+		} else {
+			this.simApp.simGPIHigh(readerArg, ports);
+		}
+		return null;
+	}
+
+	/**
+	 * Command for changing the given GPIs to low.
+	 * 
+	 * @param intp
+	 * @return
+	 */
+	public Object _simGPILow(CommandInterpreter intp) {
+		String readerArg = intp.nextArgument();
+		if (readerArg == null || readerArg.equals("")) {
+			intp.println("Usage: gpiLow <readerID> <port1> "
+					+ "[port2] [port3] ...");
+			return null;
+		}
+
+		Set<Integer> ports = new HashSet<Integer>();
+		boolean done = false;
+		while (!done) {
+			String nextArg = intp.nextArgument();
+			if (nextArg != null && !nextArg.equals("")) {
+
+				Integer port = Integer.parseInt(nextArg);
+				ports.add(port);
+			} else {
+				done = true;
+			}
+		}
+		if (ports.isEmpty()) {
+			intp.println("Usage: gpiLow <readerID> <port1> "
+					+ "[port2] [port3] ...");
+			return null;
+		} else {
+			this.simApp.simGPILow(readerArg, ports);
+		}
+		return null;
+	}
+
+	/**
+	 * The command object for the GPIFlashHigh command. This command will flash
+	 * the GPIs high for a few seconds, and then return them to low.
+	 * 
+	 * @param intp
+	 * @return
+	 */
+	public Object _simGPIFlashHigh(CommandInterpreter intp) {
+		String readerArg = intp.nextArgument();
+		if (readerArg == null || readerArg.equals("")) {
+			intp.println("Usage: gpiFlashHigh <readerID> <seconds> <port1> "
+					+ "[port2] [port3] ...");
+			return null;
+		}
+
+		String secondString = intp.nextArgument();
+		if (secondString == null || secondString.equals("")) {
+			intp.println("Usage: gpiFlashHigh <readerID> <seconds> <port1> "
+					+ "[port2] [port3] ...");
+			return null;
+		}
+		Integer seconds;
+		try {
+			seconds = Integer.parseInt(secondString);
+		} catch (NumberFormatException e) {
+			intp.println("Usage: gpiFlashHigh <readerID> <seconds> <port1> "
+					+ "[port2] [port3] ...");
+			return null;
+		}
+
+		Set<Integer> ports = new HashSet<Integer>();
+		boolean done = false;
+		while (!done) {
+			String nextArg = intp.nextArgument();
+			if (nextArg != null && !nextArg.equals("")) {
+
+				Integer port = Integer.parseInt(nextArg);
+				ports.add(port);
+			} else {
+				done = true;
+			}
+		}
+		if (ports.isEmpty()) {
+			intp.println("Usage: gpiFlashHigh <readerID> <port1> "
+					+ "[port2] [port3] ...");
+			return null;
+		} else {
+			this.simApp.simGPIFlashHigh(readerArg, seconds, ports);
+		}
+		return null;
+	}
+
+	/**
+	 * The command object for the GPIFlashLow command. This command will flash
+	 * the GPIs low for a few seconds, and then return them to high.
+	 * 
+	 * @param intp
+	 * @return
+	 */
+	public Object _simGPIFlashLow(CommandInterpreter intp) {
+		String readerArg = intp.nextArgument();
+		if (readerArg == null || readerArg.equals("")) {
+			intp.println("Usage: gpiFlashLow <readerID> <seconds> <port1> "
+					+ "[port2] [port3] ...");
+			return null;
+		}
+
+		String secondString = intp.nextArgument();
+		if (secondString == null || secondString.equals("")) {
+			intp.println("Usage: gpiFlashLow <readerID> <seconds> <port1> "
+					+ "[port2] [port3] ...");
+			return null;
+		}
+		Integer seconds;
+		try {
+			seconds = Integer.parseInt(secondString);
+		} catch (NumberFormatException e) {
+			intp.println("Usage: gpiFlashLow <readerID> <seconds> <port1> "
+					+ "[port2] [port3] ...");
+			return null;
+		}
+
+		Set<Integer> ports = new HashSet<Integer>();
+		boolean done = false;
+		while (!done) {
+			String nextArg = intp.nextArgument();
+			if (nextArg != null && !nextArg.equals("")) {
+
+				Integer port = Integer.parseInt(nextArg);
+				ports.add(port);
+			} else {
+				done = true;
+			}
+		}
+		if (ports.isEmpty()) {
+			intp.println("Usage: gpiFlashHigh <readerID> <port1> "
+					+ "[port2] [port3] ...");
+			return null;
+		} else {
+			this.simApp.simGPIFlashLow(readerArg, seconds, ports);
+		}
+		return null;
+	}
+
+	@Override
+	public String getHelp() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
