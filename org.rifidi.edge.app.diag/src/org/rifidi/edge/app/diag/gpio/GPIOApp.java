27d9e1f4eb6df21c8f35141af903f0fefa453f9c
matt <matt@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2010-03-16 18:29:26 +0000

    Made GPIO generic instead of Alien specific.

--- org.rifidi.edge.app.diag/src/org/rifidi/edge/app/diag/gpio/GPIOApp.java
+++ org.rifidi.edge.app.diag/src/org/rifidi/edge/app/diag/gpio/GPIOApp.java
@@ -3,58 +3,108 @@
  */
 package org.rifidi.edge.app.diag.gpio;
 
+import java.util.Dictionary;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.rifidi.edge.core.sensors.exceptions.CannotExecuteException;
 import org.rifidi.edge.core.sensors.management.AbstractGPIOService;
 
 /**
- * @author kyle
  * 
+ * 
+ * @author kyle
+ * @author matt
  */
-public class GPIOApp{
+public class GPIOApp {
 
-	private AbstractGPIOService<?> gpioService;
+	private static final Log logger = LogFactory.getLog(GPIOApp.class);
+
+	private final Set<AbstractGPIOService<?>> gpioServiceList = new CopyOnWriteArraySet<AbstractGPIOService<?>>();
 
 	public void start() {
-
+		logger.debug("Starting GPIOApp");
 	}
 
 	public void stop() {
-
+		logger.debug("Stopping GPIOApp");
 	}
 
 	public void setGPO(String readerID, Set<Integer> ports)
 			throws CannotExecuteException {
 		Set<Integer> translatedPorts = new HashSet<Integer>();
-		for(Integer port : ports){
+		for (Integer port : ports) {
 			translatedPorts.add(port - 1);
 		}
-		gpioService.setGPO(readerID, translatedPorts);
-
+		for (AbstractGPIOService<?> service : this.gpioServiceList) {
+			if (service.isReaderAvailable(readerID)) {
+				service.setGPO(readerID, translatedPorts);
+			}
+		}
 	}
 
 	public void flashGPO(String readerID, int port, int seconds)
 			throws CannotExecuteException {
-		port = port-1;
+		port = port - 1;
 		Set<Integer> bit = new HashSet<Integer>();
 		bit.add(port);
-		gpioService.flashGPO(readerID, seconds, bit);
+		for (AbstractGPIOService<?> service : this.gpioServiceList) {
+			if (service.isReaderAvailable(readerID)) {
+				service.flashGPO(readerID, seconds, bit);
+			}
+		}
 	}
 
 	public boolean testGPI(String readerID, int port)
 			throws CannotExecuteException {
-		port = port -1;
-		return gpioService.testGPI(readerID, port);
+		port = port - 1;
+		for (AbstractGPIOService<?> service : this.gpioServiceList) {
+			if (service.isReaderAvailable(readerID)) {
+				service.testGPI(readerID, port);
+			}
+		}
+		throw new CannotExecuteException();
 	}
 
 	/**
 	 * @param gpioService
 	 *            the gpioService to set
 	 */
-	public void setGpioService(AbstractGPIOService<?> gpioService) {
-		this.gpioService = gpioService;
+	public void onBind(AbstractGPIOService<?> gpioService,
+			Dictionary<String, String> parameters) {
+		logger.debug("Binding: " + gpioService);
+		this.gpioServiceList.add(gpioService);
 	}
 
+	/**
+	 * @param gpioService
+	 *            the gpioService to set
+	 */
+	public void onUnbind(AbstractGPIOService<?> gpioService,
+			Dictionary<String, String> parameters) {
+		this.gpioServiceList.remove(gpioService);
+	}
+
+	/**
+	 * Called by spring.
+	 * 
+	 * @param gpioSet
+	 */
+	public void setServiceSet(Set<AbstractGPIOService<?>> gpioSet) {
+		// Should we clear the local list before we iterate through the loop?
+		for (AbstractGPIOService<?> service : gpioSet) {
+			logger.debug("Adding a GPIO service in the setter: " + service);
+			this.gpioServiceList.add(service);
+		}
+	}
+
+	/**
+	 * @return
+	 */
+	public Set<AbstractGPIOService<?>> getServiceSet() {
+		return this.gpioServiceList;
+	}
 }
