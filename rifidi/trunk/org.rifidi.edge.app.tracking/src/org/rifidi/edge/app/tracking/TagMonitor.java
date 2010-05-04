dca94f8b4547fa1e49fb9aa518ea6026b6a9efa7
kyle <kyle@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2010-05-04 18:39:00 +0000

    Update To Tracking App. Now uses App API

---
+++ rifidi/trunk/org.rifidi.edge.app.tracking/src/org/rifidi/edge/app/tracking/TagMonitor.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package org.rifidi.edge.app.tracking;
+
+import org.rifidi.edge.core.app.api.JMSRifidiApp;
+import org.rifidi.edge.core.services.notification.data.TagReadEvent;
+
+import com.espertech.esper.client.EPServiceProvider;
+import com.espertech.esper.client.EPStatement;
+import com.espertech.esper.client.EventBean;
+import com.espertech.esper.client.StatementAwareUpdateListener;
+
+/**
+ * Sends tags out over jms with this format:
+ * 
+ * tag:12345|time:1234556899|readerid:alien_1|antennaid:2|rssi:2.4
+ * 
+ * @author kyle
+ * 
+ */
+public class TagMonitor extends JMSRifidiApp {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.rifidi.edge.core.app.api.RifidiApp#start()
+	 */
+	@Override
+	public void start() {
+		EPStatement query = esperService.getProvider().getEPAdministrator()
+				.createEPL(
+						"select * from ReadCycle[select * from tags]");
+		statements.add(query);
+		query.addListener(new StatementAwareUpdateListener() {
+
+			@Override
+			public void update(EventBean[] arg0, EventBean[] arg1,
+					EPStatement arg2, EPServiceProvider arg3) {
+				if (arg0 != null) {
+					for (EventBean bean : arg0) {
+						TagReadEvent event = (TagReadEvent) bean
+								.getUnderlying();
+						sendTextMessage(formTagMessage(event));
+					}
+				}
+
+			}
+		});
+
+	}
+
+	private String formTagMessage(TagReadEvent event) {
+		StringBuffer buffer = new StringBuffer();
+		buffer.append("tag:" + event.getTag().getFormattedID());
+		buffer.append("|time:" + event.getTimestamp());
+		buffer.append("|readerid:" + event.getReaderID());
+		buffer.append("|antennaid:" + event.getAntennaID());
+		if (event.getExtraInformation().containsKey(TagReadEvent.RSSI))
+			buffer.append("|rssi:"
+					+ event.getExtraInformation().get(TagReadEvent.RSSI));
+		
+		return buffer.toString();
+	}
+
+}
--- rifidi/trunk/org.rifidi.edge.app.tracking/src/org/rifidi/edge/app/tracking/TrackingApp.java
+++ rifidi/trunk/org.rifidi.edge.app.tracking/src/org/rifidi/edge/app/tracking/TrackingApp.java
@@ -3,145 +3,91 @@
  */
 package org.rifidi.edge.app.tracking;
 
-import java.util.Set;
-import java.util.concurrent.CopyOnWriteArraySet;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.rifidi.edge.app.tracking.domain.RFIDEvent;
-import org.rifidi.edge.core.services.esper.EsperManagementService;
-
-import com.espertech.esper.client.EPServiceProvider;
-import com.espertech.esper.client.EPStatement;
-import com.espertech.esper.client.EventBean;
-import com.espertech.esper.client.StatementAwareUpdateListener;
+import org.rifidi.edge.core.app.api.JMSRifidiApp;
+import org.rifidi.edge.core.app.api.service.monitoring.ReadZoneMonitoringService;
+import org.rifidi.edge.core.app.api.service.monitoring.ReadZoneSubscriber;
+import org.rifidi.edge.core.services.notification.data.TagReadEvent;
 
 /**
- * This class controls the lifecycle of the Rifidi Application. It handles the
- * submission of esper statements and handles esper events.
+ * This class keeps track of arrival and departure events.
  * 
  * @author Kyle Neumeier - kyle@pramari.com
  * 
  */
-public class TrackingApp {
+public class TrackingApp extends JMSRifidiApp implements ReadZoneSubscriber {
 
-	/** Esper service */
-	private volatile EsperManagementService esperService;
-	/** All statements that have been defined so far */
-	private final Set<EPStatement> statements = new CopyOnWriteArraySet<EPStatement>();
 	/** The Data Access Object */
-	private volatile JMSSender jmsSender;
+	private volatile TrackingMessageFactory messageFactory;
 	/** The logger for this class */
 	private static Log logger = LogFactory.getLog(TrackingApp.class);
+	/** The service that monitors read zones. */
+	private volatile ReadZoneMonitoringService monitoringService;
 
-
-	/**
-	 * Start the application. This method submits the esper statements to the
-	 * esper runtime.
+	/*
+	 * (non-Javadoc)
 	 * 
-	 * This method is called by spring
+	 * @see org.rifidi.edge.core.app.api.RifidiApp#start()
 	 */
+	@Override
 	public void start() {
-		if (jmsSender == null || esperService == null) {
-			logger.warn("Cannot start DBApp. "
-					+ "Either DAO or EsperService is null");
-		}
-		
-		//esper statement that creates a window. There are three pieces of 
-		//information in each column: tagID, readerID, and antennaID
-		statements.add(esperService.getProvider().getEPAdministrator().createEPL(
-			"create window tags.std:firstunique(tag_ID, reader_ID, antenna) " +
-				"(reader_ID String, antenna int, tag_ID String)"));
-		
-		//esper statement taht adds information to the window
-		statements.add(esperService.getProvider().getEPAdministrator().createEPL(
-				"on ReadCycle[select * from tags]" +
-				"insert into tags select readerID as reader_ID, " +
-				"antennaID as antenna, cast(tag.epc?, String) as tag_ID"));
-		
-		//esper statement that removes rows from the window if the tag has not been seen at 
-		//the antenna in the last 2 seconds
-		statements.add(esperService.getProvider().getEPAdministrator().createEPL(
-				"on pattern [every tag=tags ->" +
-					"(timer:interval(2 sec) and not tags" +
-						"(tag_ID = tag.tag_ID, reader_ID=tag.reader_ID, antenna=tag.antenna))]" +
-				"delete from tags where " +
-						"tag_ID = tag.tag_ID AND reader_ID=tag.reader_ID AND antenna=tag.antenna"));
-		
-
-		// esper statement that listens to add and remove events from the window
-		EPStatement queryAllTags = esperService.getProvider()
-				.getEPAdministrator().createEPL(
-						"select irstream * from tags");
-		
-		// add a listener to the above statement
-		queryAllTags.addListener(new StatementAwareUpdateListener() {
-
-			@Override
-			public void update(EventBean[] arg0, EventBean[] arg1,
-					EPStatement arg2, EPServiceProvider arg3) {
-				
-				//all additions
-				if (arg0 != null) {
-					for (EventBean b : arg0) {
-						String readerID = (String)b.get("reader_ID");
-						Integer antennaID = (Integer)b.get("antenna");
-						String epc = (String)b.get("tag_ID");
-						RFIDEvent e = new RFIDEvent();
-						e.setAntenna(antennaID);
-						e.setId(epc);
-						e.setReader(readerID);
-						jmsSender.arrvied(e);
-					}
-				}
-				//all deletions
-				if (arg1 != null) {
-					for (EventBean b : arg1) {
-						String readerID = (String)b.get("reader_ID");
-						Integer antennaID = (Integer)b.get("antenna");
-						String epc = (String)b.get("tag_ID");
-						RFIDEvent e = new RFIDEvent();
-						e.setAntenna(antennaID);
-						e.setId(epc);
-						e.setReader(readerID);
-						jmsSender.departed(e);
-					}
-				}
-
-			}
-		});
-		statements.add(queryAllTags);
+		monitoringService.subscribe(this);
 	}
 
-	/**
-	 * Called by spring
-	 * Iterate through all statements and stop them.
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.rifidi.edge.core.app.api.RifidiApp#stop()
 	 */
+	@Override
 	public void stop() {
-		for (EPStatement statement : statements) {
-			statement.destroy();
-		}
+		super.stop();
+		monitoringService.unsubscribe(this);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.rifidi.edge.core.app.api.service.monitoring.ReadZoneSubscriber#tagArrived
+	 * (org.rifidi.edge.core.services.notification.data.TagReadEvent)
+	 */
+	@Override
+	public void tagArrived(TagReadEvent event) {
+		sendTextMessage(messageFactory.getArrviedMessage(event));
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @seeorg.rifidi.edge.core.app.api.service.monitoring.ReadZoneSubscriber#
+	 * tagDeparted(org.rifidi.edge.core.services.notification.data.TagReadEvent)
+	 */
+	@Override
+	public void tagDeparted(TagReadEvent event) {
+		sendTextMessage(messageFactory.getDepartedMessage(event));
+
 	}
 
 	/**
 	 * Called by spring
 	 * 
-	 * @param esperService
-	 *            the esperService to set
+	 * @param service
+	 *            The ReadZoneMonitoringService
 	 */
-	public void setEsperService(EsperManagementService esperService) {
-		this.esperService = esperService;
-
+	public void setReadZoneMonitoringService(ReadZoneMonitoringService service) {
+		this.monitoringService = service;
 	}
 
 	/**
 	 * Called by spring
 	 * 
-	 * @param jmsSender
-	 *            the rfidDAO to set
+	 * @param messageFactory
 	 */
-	public void setJmsSender(JMSSender jmsSender) {
-		this.jmsSender = jmsSender;
+	public void setTrackingMessageFactory(TrackingMessageFactory messageFactory) {
+		this.messageFactory = messageFactory;
 	}
 
 }
