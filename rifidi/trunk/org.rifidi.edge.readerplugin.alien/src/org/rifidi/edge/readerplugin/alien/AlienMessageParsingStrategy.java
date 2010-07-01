0e3a938b6f4f8187276d73715d4aed06a17b998c
matt <matt@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2010-07-01 15:57:05 +0000

    Fixed the AlienMessageParsingStrategy so that it no longer creates a new array every time a new character comes back from the reader.

--- rifidi/trunk/org.rifidi.edge.readerplugin.alien/src/org/rifidi/edge/readerplugin/alien/AlienMessageParsingStrategy.java
+++ rifidi/trunk/org.rifidi.edge.readerplugin.alien/src/org/rifidi/edge/readerplugin/alien/AlienMessageParsingStrategy.java
@@ -16,6 +16,8 @@
 package org.rifidi.edge.readerplugin.alien;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.rifidi.edge.core.sensors.sessions.MessageParsingStrategy;
 
@@ -31,10 +33,11 @@ import org.rifidi.edge.core.sensors.sessions.MessageParsingStrategy;
  */
 public class AlienMessageParsingStrategy implements MessageParsingStrategy {
 
-	/** The message currently being processed. */
-	private byte[] _messageBuilder = new byte[0];
+	
 	/** Character that terminates a message from alien. */
 	public static final char TERMINATION_CHAR = '\0';
+	/** The message currently being processed. */
+	private List<Byte> builder = new LinkedList<Byte>();
 
 	/*
 	 * (non-Javadoc)
@@ -45,13 +48,16 @@ public class AlienMessageParsingStrategy implements MessageParsingStrategy {
 	@Override
 	public byte[] isMessage(byte message) {
 		if (TERMINATION_CHAR == message) {
-			byte[] ret = _messageBuilder;
-			_messageBuilder = new byte[0];
+			byte[] ret = new byte[builder.size()];
+			int index=0;
+			for(Byte b:builder) {
+				ret[index]=b;
+				index++;
+			}
+			builder.clear();
 			return ret;
 		}
-		_messageBuilder = Arrays.copyOf(_messageBuilder,
-				_messageBuilder.length + 1);
-		_messageBuilder[_messageBuilder.length - 1] = message;
+		builder.add(message);
 		return null;
 	}
 
