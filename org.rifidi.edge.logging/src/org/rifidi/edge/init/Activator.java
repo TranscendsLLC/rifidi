9b92f2e28a16eaadbdc7eb5667cfe4962185acab
kyle <kyle@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2010-08-06 20:39:45 +0000

    -Allow you to change the rifidi home directory using the org.rifidi.home property

--- org.rifidi.edge.logging/src/org/rifidi/edge/init/Activator.java
+++ org.rifidi.edge.logging/src/org/rifidi/edge/init/Activator.java
@@ -1,4 +1,4 @@
-package org.rifidi.edge.logging;
+package org.rifidi.edge.init;
 
 import java.io.File;
 import java.net.URI;
@@ -8,9 +8,16 @@ import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
 /**
- * This bundle looks for a system property called 'org.rifidi.edge.logging'
- * which contains the path to a log4j properties file. If the file cannot be
- * found, it uses the default log4j configuration in the classpath.
+ * 
+ * This bundle is the initialization bundle for the edge server. It sets up
+ * paths and variables needed by other parts of the system.
+ * 
+ * It sets the 'org.rifidi.home' variable to the the 'user.dir' variable if
+ * 'org.rifidi.home' is not already set.
+ * 
+ * It also looks for a system property called 'org.rifidi.edge.logging' which contains
+ * the path to a log4j properties file. If the file cannot be found, it uses the
+ * default log4j configuration in the classpath.
  * 
  * This bundle should be started along with the log4j bundle at start level 2 to
  * ensure that everything is logged properly.
@@ -22,11 +29,24 @@ public class Activator implements BundleActivator {
 
 	@Override
 	public void start(BundleContext arg0) throws Exception {
-		System.out.println("START LOGGING BUNDLE");
+		System.out.println("START RIFIDI INITIALIZTION BUNDLE");
+
+		// look up the path to the rifidi home directory
+		String rifidiHome = System.getProperty("org.rifidi.home");
+
+		// if it's null, set it to the "user.dir" directory
+		if (rifidiHome == null) {
+			String userDir = System.getProperty("user.dir");
+			System.setProperty("org.rifidi.home", userDir);
+		}
 		System.out.println("ALL RIFIDI CONFIGURATION PATHS RELATIVE TO : "
-				+ System.getProperty("user.dir"));
+				+ System.getProperty("org.rifidi.home"));
+
+		// set the path to the applications folder
 		System.setProperty("org.rifidi.edge.applications", "applications");
-		String s = "file:" + System.getProperty("user.dir")
+
+		// set up logging
+		String s = "file:" + System.getProperty("org.rifidi.home")
 				+ System.getProperty("file.separator")
 				+ System.getProperty("org.rifidi.edge.logging");
 		URI uri = new URI(s);
