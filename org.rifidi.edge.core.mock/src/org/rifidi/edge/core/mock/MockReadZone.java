757cb2ebe8d7d651ef28ae93fb6d59fcb9798490
james <james@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2010-10-14 17:05:38 +0000

    moved code trunk

---
+++ org.rifidi.edge.core.mock/src/org/rifidi/edge/core/mock/MockReadZone.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package org.rifidi.edge.core.mock;
+
+import java.util.List;
+
+import org.rifidi.edge.core.app.api.service.tagmonitor.ReadZone;
+
+/**
+ * @author percent
+ *
+ */
+public class MockReadZone extends ReadZone {
+
+	/**
+	 * @param readerID
+	 */
+	public MockReadZone(String readerID) {
+		super(readerID);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param readerID
+	 * @param antennas
+	 */
+	public MockReadZone(String readerID, Integer... antennas) {
+		super(readerID, antennas);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param readerID
+	 * @param tagPatterns
+	 * @param include
+	 * @param antennas
+	 */
+	public MockReadZone(String readerID, List<String> tagPatterns,
+			boolean include, Integer... antennas) {
+		super(readerID, tagPatterns, include, antennas);
+		// TODO Auto-generated constructor stub
+	}
+
+}
