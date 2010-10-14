44abab0e2cdcbcbb357b814e6449298839b0fee9
james <james@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2010-10-14 17:05:38 +0000

    moved code trunk

---
+++ rifidi/trunk/org.rifidi.edge.core.mock/src/org/rifidi/edge/core/mock/MockDatacontainerEvent.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package org.rifidi.edge.core.mock;
+
+import java.math.BigInteger;
+
+import org.rifidi.edge.core.services.notification.DatacontainerEvent;
+
+public class MockDatacontainerEvent extends DatacontainerEvent {
+
+	public MockDatacontainerEvent() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see org.rifidi.edge.core.services.notification.DatacontainerEvent#getFormattedID()
+	 */
+	@Override
+	public String getFormattedID() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.rifidi.edge.core.services.notification.DatacontainerEvent#getID()
+	 */
+	@Override
+	public BigInteger getID() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ rifidi/trunk/org.rifidi.edge.core.mock/src/org/rifidi/edge/core/mock/MockReadZoneMonitoringService.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package org.rifidi.edge.core.mock;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.rifidi.edge.core.app.api.service.tagmonitor.ReadZone;
+import org.rifidi.edge.core.app.api.service.tagmonitor.ReadZoneMonitoringService;
+import org.rifidi.edge.core.app.api.service.tagmonitor.ReadZoneSubscriber;
+/**
+ * @author percent
+ */
+
+public class MockReadZoneMonitoringService implements ReadZoneMonitoringService {
+
+	public ReadZoneSubscriber subscriber;
+	public List<ReadZone> readZones;
+	public Float departureTime;
+	public TimeUnit timeUnit;	
+	public boolean subscribed;
+	
+	public MockReadZoneMonitoringService() {
+		this.subscribed = false;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.rifidi.edge.core.app.api.service.tagmonitor.ReadZoneMonitoringService#subscribe(org.rifidi.edge.core.app.api.service.tagmonitor.ReadZoneSubscriber)
+	 */
+	@Override
+	public void subscribe(ReadZoneSubscriber subscriber) {
+		assert false;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.rifidi.edge.core.app.api.service.tagmonitor.ReadZoneMonitoringService#subscribe(org.rifidi.edge.core.app.api.service.tagmonitor.ReadZoneSubscriber, java.util.List, java.lang.Float, java.util.concurrent.TimeUnit)
+	 */
+	@Override
+	public void subscribe(ReadZoneSubscriber subscriber,
+			List<ReadZone> readZones, Float departureTime, TimeUnit timeUnit) {
+		this.subscriber = subscriber;
+		this.readZones = readZones;
+		this.departureTime = departureTime;
+		this.timeUnit = timeUnit;
+		subscribed = true;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.rifidi.edge.core.app.api.service.tagmonitor.ReadZoneMonitoringService#subscribe(org.rifidi.edge.core.app.api.service.tagmonitor.ReadZoneSubscriber, org.rifidi.edge.core.app.api.service.tagmonitor.ReadZone, java.lang.Float, java.util.concurrent.TimeUnit)
+	 */
+	@Override
+	public void subscribe(ReadZoneSubscriber subscriber, ReadZone readZone,
+			Float departureTime, TimeUnit timeUnit) {
+		assert false;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.rifidi.edge.core.app.api.service.tagmonitor.ReadZoneMonitoringService#unsubscribe(org.rifidi.edge.core.app.api.service.tagmonitor.ReadZoneSubscriber)
+	 */
+	@Override
+	public void unsubscribe(ReadZoneSubscriber subscriber) {
+		assert subscribed == true && this.subscriber == subscriber;
+		assert false;
+
+	}
+}
