46ffd756de84e61bf120e760b364a5ebb34740ae
james <james@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2010-11-03 02:49:07 +0000

    Fixed [another] version bug.

--- build/conf.py
+++ build/conf.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
 
-jar_path = ['../../Rifidi-SDK/lib/' ]
-src_path = ['../../org.rifidi.edge.init', '../../org.rifidi.edge.api',\
-            '../../org.rifidi.edge.core']
+jar_path = ['../Rifidi-SDK/lib/' ]
+src_path = ['../org.rifidi.edge.core']
