0ce5eacdee5736a6520b840d2c3fc72b62bfcbb0
james <james@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2010-11-16 22:11:45 +0000

    Working version.

--- build/export.py
+++ build/export.py
@@ -5,107 +5,121 @@ import re
 import subprocess
 from os.path import join, abspath
 
-launchers = ['../Rifidi-SDK/eclipse-libs/plugins/']
+launchers = ['../Rifidi-SDK/launchers']
 
-lib = 'lib/'
+lib = 'lib'
 deploy = 'deploy1/'
-sources = ['export/linux.gtk.x86_64/']
-configurations = ['ambient-edge']
+exports = 'export/'
+configurations = ['ambient-edge', 'edge']
 
-class Builder:
-    def __init__(self, launchers, sources, configurations, lib, deploy):
-        self.launchers = launchers
-        self.sources = sources
-        self.configurations = configurations
-        self.lib = lib
-        self.deploy = deploy
-        
-    def copy_binaries(self, root, file, type):
-        print 'binaries'
-        if type == 'dir':
-            self.cdir = join(self.lib, file)
-            ret = subprocess.call(['mkdir', self.cdir])
-            assert ret == 0
-        elif type == 'file':
-            ret = subprocess.call(['cp', join(root, file), join(self.cdir, file)])
-            print 'file'
-        else:
-            assert False
-            
-    def copy_configs(self, root, file, type):
-        print 'configs'
-        if type == 'dir':
-            print 'dir'
-        elif type == 'file':
-            print 'file'    
-
-    def copy(self, source, handler):
-        for path in source:
-            for root, dirs, files in os.walk(path):
-                for dir in dirs:
-                    if re.search('.svn', join(root, dir)):
-                        continue
-                    else:
-                        print join(root,dir)
-                        handler(root, dir, 'dir')                    
-                for file in files:
-                    if re.search('.svn', join(root, file)):
-                        continue
-                    else:
-                        print join(root, file)
-                        handler(root, file, 'file')
-
-                        
-                        
-    def doit(self):
-        self.copy(launchers, self.copy_binaries)
-        #self.copy(sources, self.copy_configs)
-        
-    def load(self):
-        for root, dir, libs in self.src_manifests:
-            #print join(root, dir, 'MANIFEST.MF')
-            manifest_des = open(join(root, dir, 'MANIFEST.MF'), 'r')
-            manifest_file = manifest_des.read()
-            #print manifest_file
-            parser = manifest.ManifestParser()
-            bundle = parser.parse(manifest_file)
-            bundle.root = root
-            #print bundle, bundle.sym_name
-            if libs.keys().__len__() > 0:
-                print libs
-                bundle.extra_libs = libs
-                #assert False
-            self.bundles.append(bundle)
-            
-    def display(self):
-        for i in self.bundles:
-            i.display()
-            print '-'*80
-        
-    def find(self, src_path):
-        for i in src_path:
-            for root, dirs, files in os.walk(i):
-                
-                libs = {}
-                manifest = ()
-                manifest_found = False
-                
-                if 'META-INF' in dirs:
-                    manifest_found = True
-                else:
+def copy_launchers():
+    for path in launchers:
+        for root, dirs, files in os.walk(path):
+            for dir in dirs:
+                if dir == '.svn':
                     continue
+                else:
+                    print dir
+                ret = subprocess.call(['rm', '-rf', join(lib, dir)])
+                assert ret == 0
                     
-                for dir in dirs:
-                    if dir == 'META-INF':
-                        manifest = (root, dir)
-                    if dir == 'lib':
-                        libs = self.find_libs(join(root, dir))
-                        print libs
-                        #assert False
+                print 'cp -r '+join(root,dir)+' '+join(lib,dir)
+                ret = subprocess.call(['cp','-r', join(root, dir), lib])
+                assert ret == 0
+                for sub_root, sub_dirs, sub_files in os.walk(join(lib,dir)):
+                    for sub_dir in sub_dirs:
+                        if re.search('.svn', sub_dir):
+                            ret = subprocess.call(['rm', '-rf', join(sub_root,sub_dir)])
+                            
+            break
 
-# cp -r ../Rifidi-SDK/eclipse-libs/plugins/org.eclipse.equinox.launcher.cocoa.macosx_1.0.1.R35x_v20090707 ../Rifidi-SDK/eclipse-libs/plugins/org.eclipse.equinox.launcher.cocoa.macosx.x86_64_1.0.1.R35x_v20090707 ../Rifidi-SDK/eclipse-libs/plugins/org.eclipse.equinox.launcher.gtk.linux.x86_1.0.200.v20090520 ../Rifidi-SDK/eclipse-libs/plugins/org.eclipse.equinox.launcher.gtk.linux.x86_64_1.0.200.v20090519 ../Rifidi-SDK/eclipse-libs/plugins/org.eclipse.equinox.launcher.win32.win32.x86_1.0.200.v20090519 ../Rifidi-SDK/eclipse-libs/plugins/org.eclipse.equinox.launcher.win32.win32.x86_64_1.0.200.v20090519 
-#  cp -r lib/* 
+def setup_config_dir(config):
+    ret = subprocess.call(['rm', '-rf', join(deploy, config)])
+    assert ret == 0
+    ret = subprocess.call(['mkdir', join(deploy, config)])
+    assert ret == 0    
 
+def copy_eclipse_export_artifacts(root, target, export_dir):
+    target_dir = join(target,export_dir)    
+    ret = subprocess.call(['rm', '-rf', target_dir])
+    assert ret == 0
+    print 'cp -r '+join(root,export_dir)+' '+target_dir
+    ret = subprocess.call(['cp','-r', join(root, export_dir), target])
+    assert ret == 0
+        
+    for target_root, exported_dirs, exported_files in os.walk(target_dir):
+        for exported_dir in exported_dirs:
+            if re.search('.svn', join(target_root, exported_dir)):
+                ret = subprocess.call(['rm', '-rf', join(target_root,exported_dir)])
+                
+def copy_bundles(target, export_dir):
+    target_dir = join(target,export_dir)
+    
+    for lib_root, bundle_dirs, bundles in os.walk(lib):
+        for bundle_dir in bundle_dirs:
+            assert not re.search('.svn', bundle_dir)
+            ret = subprocess.call(['cp', '-r', join(lib_root,bundle_dir),
+                             join(target_dir,'rifidi','plugins')])
+            assert ret == 0
+        for bundle in bundles:
+            assert not re.search('.svn', bundle_dir)
+            ret = subprocess.call(['cp', join(lib_root,bundle),
+                             join(target_dir,'rifidi','plugins')])
+            assert ret == 0
+        break
+
+def copy_config_dir(target, export_dir, config_root, config_dirs, config_files):
+    target_dir = join(target,export_dir)
+    
+    for config_dir in config_dirs:
+        if re.search('.svn', config_dir):
+            continue
+
+        ret = subprocess.call(['rm', '-rf', join(target_dir, 'rifidi', config_dir)])
+        assert ret == 0
+    
+        ret = subprocess.call(['cp', '-r', join(config_root,config_dir),
+                             join(target_dir, 'rifidi', config_dir)])
+        assert ret == 0
+        for config_dir_root, config_dir_dirs, config_dir_file in \
+                            os.walk(join(target_dir, 'rifidi',config_dir)):
+            for config_dir_dir in config_dir_dirs:
+                if re.search('.svn', config_dir_dir):
+                    ret = subprocess.call(['rm', '-rf', join(config_dir_root, config_dir_dir)])
+                
+    for config_file in config_files:
+        ret = subprocess.call(['rm', '-rf', join(target_dir, 'rifidi', config_file)])
+        assert ret == 0
+        ret = subprocess.call(['cp', join(config_root,config_file),
+                             join(target_dir,'rifidi', config_file)])
+        assert ret == 0
+                    
+                        
+def build_edge_server():
+    ret = subprocess.call(['ant', 'package'])
+    assert ret == 0
+
+    copy_launchers()
+
+    for config in configurations:
+        setup_config_dir(config)
+        target = join(deploy, config)
+        for root, export_dirs, files in os.walk(exports):
+            for export_dir in export_dirs:
+                if re.search('.svn', export_dir):
+                    continue
+                copy_eclipse_export_artifacts(root, target, export_dir)
+                copy_bundles(target, export_dir)
+                for config_root, config_dirs, config_files in os.walk(config):
+                    copy_config_dir(target,export_dir,
+                                    config_root, config_dirs, config_files)
+                    break
+            break
+                
+            
+        
 if __name__ == '__main__':
-    builder = Builder(launchers, sources, configurations,lib, deploy)
-    builder.doit()
+    build_edge_server()
+
+    import sys
+    sys.exit()
