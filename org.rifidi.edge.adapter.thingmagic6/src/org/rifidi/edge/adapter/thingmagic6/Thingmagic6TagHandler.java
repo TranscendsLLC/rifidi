5d106eb74a3e541e3300db2301701488e0b6320a
matt <matt@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2011-05-04 22:06:30 +0000

    Initial import, still not finished coding yet.

---
+++ org.rifidi.edge.adapter.thingmagic6/src/org/rifidi/edge/adapter/thingmagic6/Thingmagic6TagHandler.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package org.rifidi.edge.adapter.thingmagic6;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.rifidi.edge.notification.EPCGeneration2Event;
+import org.rifidi.edge.notification.ReadCycle;
+import org.rifidi.edge.notification.TagReadEvent;
+
+import com.thingmagic.ReadListener;
+import com.thingmagic.Reader;
+import com.thingmagic.TagReadData;
+import com.thingmagic.Gen2.TagData;
+
+/**
+ * 
+ * 
+ * @author Matthew Dean
+ */
+public class Thingmagic6TagHandler implements ReadListener {
+
+	public Thingmagic6SensorSession session;
+
+	public Thingmagic6TagHandler(Thingmagic6SensorSession session) {
+		this.session = session;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.thingmagic.ReadListener#tagRead(com.thingmagic.Reader, com.thingmagic.TagReadData)
+	 */
+	@Override
+	public void tagRead(Reader arg0, TagReadData arg1) {
+		System.out.println(arg1.epcString() + ", " + arg1.getTag().getProtocol());
+	}
+	
+
+	// private ReadCycle createReadCycle(TagReadData trd) {
+	// Set<TagReadEvent> tagreaderevents = new HashSet<TagReadEvent>();
+	// TagData tag = (TagData) trd.getTag();
+	// //Assuming it is a gen2 at the moment
+	//		
+	// EPCGeneration2Event gen2event = new EPCGeneration2Event();
+	// gen2event.setEPCMemory(memBank, length)
+	//		
+	// TagReadEvent tagevent = new TagReadEvent(session.getReaderID(), fdasdf,
+	// trd.getAntenna(), trd.getTime());
+	//		
+	// return new ReadCycle(tagreaderevents, session.getReaderID(), System
+	// .currentTimeMillis());
+	// }
+
+}
