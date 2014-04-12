737c69c1494bb7d37e369d3b3d723bf89e037a03
mattrifidi <mattrifidi@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2014-04-12 05:00:33 +0000

    Fixed the copyright for all java files.

--- apps/org.rifidi.app.northwind/src/org/rifidi/edge/northwind/NorthwindForkliftSubscriber.java
+++ apps/org.rifidi.app.northwind/src/org/rifidi/edge/northwind/NorthwindForkliftSubscriber.java
@@ -1,14 +1,15 @@
-/*
- *  NorthwindForkliftSubscriber.java
- *
- *  Created:	Aug 19, 2010
- *  Project:	Rifidi Edge Server - A middleware platform for RFID applications
- *  				http://www.rifidi.org
- *  				http://rifidi.sourceforge.net
- *  Copyright:	Pramari LLC and the Rifidi Project
- *  License:	GNU Public License (GPL)
- *  				http://www.opensource.org/licenses/gpl-3.0.html
- */
+/*******************************************************************************
+ * Copyright (c) 2014 Transcends, LLC.
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU 
+ * General Public License as published by the Free Software Foundation; either version 2 of the 
+ * License, or (at your option) any later version. This program is distributed in the hope that it will 
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You 
+ * should have received a copy of the GNU General Public License along with this program; if not, 
+ * write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, 
+ * USA. 
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ *******************************************************************************/
 package org.rifidi.edge.northwind;
 
 import java.util.Set;
