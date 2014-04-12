2f3ecdd1c82aacd2252233b07fec9796d4845d58
mattrifidi <mattrifidi@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2014-04-12 05:00:33 +0000

    Fixed the copyright for all java files.

--- rifidi/trunk/org.rifidi.edge.adapter.awid/src/org/rifidi/edge/adapter/awid/awid2010/Awid2010SensorFactory.java
+++ rifidi/trunk/org.rifidi.edge.adapter.awid/src/org/rifidi/edge/adapter/awid/awid2010/Awid2010SensorFactory.java
@@ -1,10 +1,15 @@
-/*
- * Copyright (c) 2013 Transcends, LLC.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Public License v2.0
- * which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
- */
+/*******************************************************************************
+ * Copyright (c) 2014 Transcends, LLC.
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU 
+ * General Public License as published by the Free Software Foundation; either version 2 of the 
+ * License, or (at your option) any later version. This program is distributed in the hope that it will 
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You 
+ * should have received a copy of the GNU General Public License along with this program; if not, 
+ * write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, 
+ * USA. 
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ *******************************************************************************/
 package org.rifidi.edge.adapter.awid.awid2010;
 
 import java.util.Map;
