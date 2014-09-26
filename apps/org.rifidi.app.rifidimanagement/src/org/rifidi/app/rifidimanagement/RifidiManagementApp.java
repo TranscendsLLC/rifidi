aa527826b11adbe6c01ace6c86521ca27674afe8
manueltobon1 <manueltobon1@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2014-09-26 20:37:32 +0000

    Changes to have a stableset subscriber

--- apps/org.rifidi.app.rifidimanagement/src/org/rifidi/app/rifidimanagement/RifidiManagementApp.java
+++ apps/org.rifidi.app.rifidimanagement/src/org/rifidi/app/rifidimanagement/RifidiManagementApp.java
@@ -12,8 +12,10 @@
  *******************************************************************************/
 package org.rifidi.app.rifidimanagement;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.logging.Log;
@@ -21,50 +23,99 @@ import org.apache.commons.logging.LogFactory;
 import org.rifidi.edge.api.AbstractRifidiApp;
 import org.rifidi.edge.api.service.tagmonitor.ReadZone;
 import org.rifidi.edge.api.service.tagmonitor.ReadZoneMonitoringService;
+import org.rifidi.edge.api.service.tagmonitor.StableSetService;
+import org.rifidi.edge.daos.ReaderDAO;
+import org.rifidi.edge.sensors.AbstractSensor;
+import org.rifidi.edge.sensors.SensorSession;
 
 /**
  * A simple application to test available rifidi API commands
  * 
  * @author Manuel Tobon - manuel@transcends.co
  */
-public class RifidiManagementApp 
-		extends AbstractRifidiApp {
+public class RifidiManagementApp extends AbstractRifidiApp {
 
-	/** The readZoneMonitoring service: Monitors a ReadZone or group of ReadZones 
-	 * and reports when a tag enters or leaves a reader or antenna in the readzone */
-	private ReadZoneMonitoringService readZoneMonitoringService;
-	
-	// A list of all readzoneSubscribers that is kept for when the time comes to
-	// unsubscribe them.
-	private List<MyReadZoneSubscriber> readZoneMonitoringSubscriberList;
-	
-	/** departureTime: If this amount of time in seconds passes since the last 
-	 * time a tag has been seen, then fire a departure event.
+	/**
+	 * The stableSet service: returns a list of tags representing a "group"
+	 * after an allotted time has passed with no new tags arriving. For
+	 * instance, if you place 30 tags in the readzone of the reader, assuming
+	 * all tags can be read correctly it will return a list of 30 tags assuming
+	 * no new tags are seen for the duration given
 	 */
-	private Float departureTime;
-	
+	private StableSetService stableSetService;
+
+	/** Reader DAO -- used to stop reader sessions */
+	/**
+	 * Injected from spring, if you need to stop reader sessions for a
+	 * particular reader
+	 **/
+	private ReaderDAO readerDAO;
+
+	// A list of all stableSetsubscribers that is kept for when the time comes
+	// to
+	// unsubscribe them.
+	private List<MyStableSetSubscriber> stableSetSubscriberList;
+
+	/**
+	 * stableSetTime - The time that must pass with no new tags having been seen
+	 * before the stable set will return. It's used for stableSet Service
+	 */
+	private Float stableSetTime;
+
 	/** Logger for this class */
 	private final Log logger = LogFactory.getLog(getClass());
-		
 
-	/**
-	 * @return the departureTime
-	 */
-	private Float getDepartureTime() {
-		return departureTime;
-	}
-
-	/**
-	 * @param departureTime the departureTime to set
-	 */
-	private void setDepartureTime(Float departureTime) {
-		this.departureTime = departureTime;
-	}
-	
-	
-	
 	public RifidiManagementApp(String group, String name) {
 		super(group, name);
+	}
+
+	/**
+	 * @return the stableSetService
+	 */
+	public StableSetService getStableSetService() {
+		return stableSetService;
+	}
+
+	/**
+	 * @param stableSetService
+	 *            the stableSetService to set
+	 */
+	public void setStableSetService(StableSetService stableSetService) {
+		this.stableSetService = stableSetService;
+	}
+
+	/**
+	 * Inject the reader DAO to allow this app to stop the reader.
+	 * 
+	 * @param readerDAO
+	 */
+	// Used by spring framework to inject the reader dao
+	public void setReaderDAO(ReaderDAO readerDAO) {
+		this.readerDAO = readerDAO;
+	}
+
+	/**
+	 * @return the readerDAO
+	 */
+	private ReaderDAO getReaderDAO() {
+		return readerDAO;
+	}
+
+	/**
+	 * @return the stableSetTime
+	 */
+
+	public Float getStableSetTime() {
+		return stableSetTime;
+	}
+
+	/**
+	 * @param stableSetTime
+	 *            the stableSetTime to set
+	 */
+
+	public void setStableSetTime(Float stableSetTime) {
+		this.stableSetTime = stableSetTime;
 	}
 
 	/*
@@ -75,14 +126,27 @@ public class RifidiManagementApp
 	@Override
 	public void _start() {
 		super._start();
-		
-		readZoneMonitoringSubscriberList = new LinkedList<MyReadZoneSubscriber>();
-		
-		// Get the read zone.
-		ReadZone observableReadZone = super.getReadZones().get("observable");
 
-		//Subscribe the observable read zone readZoneMonitoringService
-		subscribeToReadZoneService(observableReadZone);
+		stableSetSubscriberList = new LinkedList<MyStableSetSubscriber>();
+
+		// Get all the read zones
+		HashMap<String, ReadZone> allReadZones = super.getReadZones();
+
+		// Get the front door read zone. According to this readzone properties
+		// file,
+		// Front_Door readzone contains only one reader
+		ReadZone frontDoorReadZone = allReadZones.get("Front_Door");
+
+		// Get the back door read zone. According to this readzone properties
+		// file,
+		// Back_Door readzone contains one or many readers
+		ReadZone backDoorReadZone = allReadZones.get("Back_Door");
+
+		// Subscribe the frontDoorReadZone to stableSetService
+		subscribeToStableSetService(frontDoorReadZone);
+
+		// Subscribe the backDoorReadZone to stableSetService
+		subscribeToStableSetService(backDoorReadZone);
 	}
 
 	/*
@@ -92,10 +156,10 @@ public class RifidiManagementApp
 	 */
 	@Override
 	public void _stop() {
-		
-		//Unsubscribe from readzone service
-		for (MyReadZoneSubscriber sub : this.readZoneMonitoringSubscriberList) {
-			unsubscribeFromReadZoneService(sub);
+
+		// Unsubscribe from stableset service
+		for (MyStableSetSubscriber sub : this.stableSetSubscriberList) {
+			unsubscribeFromStableSetService(sub);
 		}
 	}
 
@@ -106,65 +170,75 @@ public class RifidiManagementApp
 	 */
 	@Override
 	public void initialize() {
-		
-		//Set departure time from properties
-		setDepartureTime( Float.parseFloat(getProperty("departureTime",
-				null)) );
+
+		// Set stableset time from properties
+		setStableSetTime(Float.parseFloat(getProperty("stableSetTime", null)));
 
 	}
 
 	/**
-	 * Called by spring. This method injects the ReadZoneMonitoringService into
-	 * the application.
+	 * Stop any currently running sessions for the given reader ID.
 	 * 
-	 * @param rzms
+	 * @param readerID
 	 */
-	public void setReadZoneMonitoringService(ReadZoneMonitoringService rzms) {
-		this.readZoneMonitoringService = rzms;
+	public void stopReaderSession(String readerID) {
+		AbstractSensor<?> sensor = getReaderDAO().getReaderByID(readerID);
+		if (sensor != null) {
+			Map<String, SensorSession> sessions = sensor.getSensorSessions();
+			for (String s : sessions.keySet()) {
+				sessions.get(s).disconnect();
+			}
+
+			logger.debug("Disconnected sessions for reader id " + readerID);
+
+		} else {
+
+			logger.error("Error disconnecting sessions for reader id "
+					+ readerID + ". No reader with this name found.");
+
+		}
 	}
-	
-	
+
 	
 	/**
-	 * @return the readZoneMonitoringService
+	 * 
+	 * @param readZone
+	 *            the readZone to subscribe
 	 */
-	public ReadZoneMonitoringService getReadZoneMonitoringService() {
-		return readZoneMonitoringService;
+	public void subscribeToStableSetService(ReadZone readZone) {
+
+		// Create an instance of the stableset subscriber
+		MyStableSetSubscriber myStableSetSubscriber = new MyStableSetSubscriber(
+				this, readZone);
+
+		stableSetSubscriberList.add(myStableSetSubscriber);
+
+		// Create a list of readzones
+		List<ReadZone> readZoneList = new LinkedList<ReadZone>();
+
+		// Add the readzone to the readzone list and subscribe
+		readZoneList.add(readZone);
+
+		getStableSetService().subscribe(myStableSetSubscriber, readZoneList,
+				getStableSetTime(), TimeUnit.SECONDS, true, true);
+
+		logger.info("subscribed readzone with reader id: "
+				+ readZone.getReaderID() + " and readerStableSetTime: "
+				+ getStableSetTime());
+
 	}
 
 	/**
-	 * Subscribe to readzone service
-	 * @param readZone the readzone to subscribe
+	 * 
+	 * @param stableSetSubscriber
 	 */
-	public void subscribeToReadZoneService(ReadZone readZone){
-		
-		//Create an instance of the stableset subscriber
-		MyReadZoneSubscriber myReadZoneSubscriber = new MyReadZoneSubscriber();
-		
-		readZoneMonitoringSubscriberList.add(myReadZoneSubscriber);
-		
-		//Create a list of readzones
-		List<ReadZone> readZoneList = new LinkedList<ReadZone>();
-		
-		//Add the readzone to the readzone list and subscribe 
-		readZoneList.add(readZone);
-		
-		getReadZoneMonitoringService().subscribe(myReadZoneSubscriber, readZoneList, 
-				getDepartureTime(), TimeUnit.SECONDS, true, true);
-		
-		logger.info("subscribed readzone with reader id: " + readZone.getReaderID()
-				+ " and departureTime: " + getDepartureTime());
-		
-	}
-	
-	
-	
-	
-public void unsubscribeFromReadZoneService(MyReadZoneSubscriber myReadZoneSubscriber){
-		
-		getReadZoneMonitoringService().unsubscribe(myReadZoneSubscriber);
-		
-		//logger.info("unsubscribed subscriber with reader id: " + myReadZoneSubscriber.getReadZone().getReaderID());
-		
+	public void unsubscribeFromStableSetService(
+			MyStableSetSubscriber myStableSetSubscriber) {
+
+		getStableSetService().unsubscribe(myStableSetSubscriber);
+
+		logger.info("unsubscribed subscriber with reader id: "
+				+ myStableSetSubscriber.getReadZone().getReaderID());
+
 	}
 }
