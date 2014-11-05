3d2c3cf3e5499483ec92090b56fd5103eac9cad9
manueltobon1 <manueltobon1@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2014-11-05 02:24:49 +0000

    app rest functions getAppProperties, getGroupProperties, setAppProperties, setGroupProperties, getReadZones, deleteReadZone, getReadZoneProperties

--- rifidi/trunk/org.rifidi.edge/src/org/rifidi/edge/util/RifidiEdgeHelper.java
+++ rifidi/trunk/org.rifidi.edge/src/org/rifidi/edge/util/RifidiEdgeHelper.java
@@ -1,5 +1,6 @@
 package org.rifidi.edge.util;
 
+import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
@@ -8,12 +9,20 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.Serializable;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.channels.FileChannel.MapMode;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
+
+import javax.management.Attribute;
+import javax.management.AttributeList;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -21,10 +30,10 @@ import org.rifidi.edge.api.service.tagmonitor.ReadZone;
 import org.rifidi.edge.exceptions.PropertiesFileNotFoundException;
 
 public class RifidiEdgeHelper implements Serializable {
-	
+
 	private static final String READZONES_FOLDER_NAME = "readzones";
-	
-	private static final String READZONE_PREFIX_FILE_NAME = "readzone";
+
+	private static final String READZONE_FILE_NAME_PREFIX = "readzone";
 
 	/** The logger for this class */
 	private static final Log logger = LogFactory.getLog(RifidiEdgeHelper.class);
@@ -87,12 +96,58 @@ public class RifidiEdgeHelper implements Serializable {
 		}
 	}
 
+	private static final void writeProperties(String fileName, byte[] data) {
+		DataOutputStream os = null;
+		try {
+			os = new DataOutputStream(new FileOutputStream(fileName));
+			os.write(data);
+			os.flush();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (os != null)
+					os.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private static final void writeReadZoneData(String group,
+			String readZoneName, byte[] data) {
+		String dataDir = getDataDirPath(group, READZONES_FOLDER_NAME);
+		String fileName = dataDir + File.separator + READZONE_FILE_NAME_PREFIX
+				+ "-" + readZoneName + ".properties";
+		DataOutputStream os = null;
+		try {
+			os = new DataOutputStream(new FileOutputStream(fileName));
+			os.write(data);
+			os.flush();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (os != null)
+					os.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
 	/**
 	 * This is a helper method that does the work of loading the read zones
 	 */
 	public static HashMap<String, ReadZone> getReadZones(String group) {
-		HashMap<String, byte[]> fileMap = getReadzoneFiles("readzone",
-				"readzones", group);
+		HashMap<String, byte[]> fileMap = getReadzoneFiles(
+				READZONE_FILE_NAME_PREFIX, READZONES_FOLDER_NAME, group);
 		HashMap<String, ReadZone> readZones = new HashMap<String, ReadZone>();
 		for (String readZoneName : fileMap.keySet()) {
 			byte[] file = fileMap.get(readZoneName);
@@ -106,42 +161,75 @@ public class RifidiEdgeHelper implements Serializable {
 		}
 		return readZones;
 	}
-	
-	
+
 	/**
 	 * Get the readzone properties
-	 * @param groupName the name of application group
-	 * @param appName the name of application
-	 * @param readZoneName the name of readzone
+	 * 
+	 * @param groupName
+	 *            the name of application group
+	 * @param appName
+	 *            the name of application
+	 * @param readZoneName
+	 *            the name of readzone
 	 * @return the readzone properties
-	 * @throws PropertiesFileNotFoundException if cannot get the readzone properties 
-	 * file
-	 * @throws IOException if cannot load the properties from file
+	 * @throws PropertiesFileNotFoundException
+	 *             if cannot get the readzone properties file
+	 * @throws IOException
+	 *             if cannot load the properties from file
 	 */
-	public static Properties getReadZoneProperties(String groupName, 
+	public static Properties getReadZoneProperties(String groupName,
 			String appName, String readZoneName)
 			throws PropertiesFileNotFoundException, IOException {
-		
-		byte[] appPropBytes = getReadZonePropertiesFile(groupName, appName, readZoneName);
+
+		byte[] appPropBytes = getReadZonePropertiesFile(groupName, appName,
+				readZoneName);
 
 		Properties properties = new Properties();
 		properties.load(new ByteArrayInputStream(appPropBytes));
 		return properties;
 	}
+	
+	/**
+	 * Deletes readzone
+	 * @param groupName group name of application
+	 * @param readZone readzone name
+	 * @throws Exception if readzone does not exist
+	 */
+	public static void deleteReadZone(String groupName, String readZone)
+			throws Exception{
+		
+		String dataPath = getDataDirPath(groupName, READZONES_FOLDER_NAME);
+		String fileName = dataPath + File.separator + READZONE_FILE_NAME_PREFIX 
+				+ "-" + readZone + ".properties";
 
+		//check if file exists
+		File file = new File(fileName);
+		if (!file.exists()){
+			//If not exists, can not delete
+			throw new Exception("Readzone " + readZone + " does not exist");
+		} else {
+			
+			//exists, delete file
+			file.delete();
+		}
+		
+	}
 
 	/**
 	 * Get the application properties
-	 * @param groupName application group name
-	 * @param appName application name
+	 * 
+	 * @param groupName
+	 *            application group name
+	 * @param appName
+	 *            application name
 	 * @return the application properties
-	 * @throws PropertiesFileNotFoundException if cannot get the application properties 
-	 * file
-	 * @throws IOException if cannot load the properties from file
+	 * @throws PropertiesFileNotFoundException
+	 *             if cannot get the application properties file
+	 * @throws IOException
+	 *             if cannot load the properties from file
 	 */
 	public static Properties getApplicationProperties(String groupName,
-			String appName) throws PropertiesFileNotFoundException,
-			IOException {
+			String appName) throws PropertiesFileNotFoundException, IOException {
 
 		byte[] appPropBytes = getApplicationPropertiesFile(groupName, appName);
 
@@ -149,18 +237,131 @@ public class RifidiEdgeHelper implements Serializable {
 		properties.load(new ByteArrayInputStream(appPropBytes));
 		return properties;
 	}
-	
+
+	/**
+	 * Set application properties
+	 * 
+	 * @param groupName
+	 *            group name of application
+	 * @param appName
+	 *            application name
+	 * @param attributes
+	 *            attributes to be set
+	 * @throws IOException
+	 *             if there is an error setting the properties
+	 */
+	public static void setApplicationProperties(String groupName,
+			String appName, AttributeList attributes) throws IOException {
+
+		String dataPath = getDataDirPath(groupName, null);
+		String fileName = dataPath + File.separator + appName + ".properties";
+		setProperties(fileName, attributes);
+
+	}
 	
 	/**
-	 * Get the properties for application group
-	 * @param groupName the name of application group
-	 * @return the properties for application group
-	 * @throws PropertiesFileNotFoundException if cannot get properties file
-	 * @throws IOException if cannot load the properties from file
+	 * Set group properties
+	 * 
+	 * @param groupName
+	 *            group name of application
+	 * @param attributes
+	 *            attributes to be set
+	 * @throws IOException
+	 *             if there is an error setting the properties
 	 */
-	public static Properties getGroupProperties(String groupName) 
-			throws PropertiesFileNotFoundException,
-			IOException {
+	public static void setGroupProperties(String groupName, AttributeList attributes) 
+			throws IOException {
+
+		String dataPath = getDataDirPath(groupName, null);
+		String fileName = dataPath + File.separator + groupName + ".properties";
+		setProperties(fileName, attributes);
+
+	}
+
+	/**
+	 * Set properties in specified properties file
+	 * 
+	 * @param fileName
+	 *            file where properties are going to be set
+	 * @param attributes
+	 *            the list of properties
+	 * @throws IOException
+	 *             if there is an error reading the file
+	 */
+	private static void setProperties(String fileName, AttributeList attributes)
+			throws IOException {
+
+		//check if file exists
+		File file = new File(fileName);
+		if (!file.exists()){
+			//If not exists, create it
+			file.createNewFile();
+		}
+		
+		List<String> fileLines = new LinkedList<String>();
+
+		InputStream ips = new FileInputStream(fileName);
+		InputStreamReader ipsr = new InputStreamReader(ips);
+		BufferedReader br = new BufferedReader(ipsr);
+		String line;
+		while ((line = br.readLine()) != null) {
+			fileLines.add(line);
+		}
+		br.close();
+
+		// Iterate properties received to see if they exist in file, and update
+		// them,
+		// or add to file if they do not exist.
+		for (Attribute attribute : attributes.asList()) {
+
+			String propName = attribute.getName();
+			boolean attributeFound = false;
+			for (String fileLine : fileLines) {
+				if (!fileLine.isEmpty() && !fileLine.startsWith("#")) {
+					String[] splittedLine = fileLine.split("=");
+					if (splittedLine.length > 0) {
+						String filePropName = splittedLine[0];
+						if (filePropName.trim().equals(propName.trim())) {
+							attributeFound = true;
+							int index = fileLines.indexOf(fileLine);
+							fileLines.set(index,
+									filePropName + "=" + attribute.getValue());
+						}
+					}
+				}
+			}
+
+			if (!attributeFound) {
+				// add new attribute
+				fileLines.add(attribute.getName() + "=" + attribute.getValue());
+			}
+
+		}
+
+		// fileLines has the attributes to be set
+		String dataToWrite = "";
+		for (String data : fileLines) {
+			dataToWrite += data + "\n";
+		}
+
+		// Write to app properties file
+		writeProperties(fileName, dataToWrite.getBytes());
+
+	}
+
+	/**
+	 * Get the properties for application group
+	 * 
+	 * @param groupName
+	 *            the name of application group
+	 * @return the properties for application group
+	 * @throws PropertiesFileNotFoundException
+	 *             if cannot get properties file
+	 * @throws IOException
+	 *             if cannot load the properties from file
+	 */
+	public static Properties getGroupProperties(String groupName)
+			throws PropertiesFileNotFoundException, IOException {
 
 		byte[] groupPropBytes = getGroupPropertiesFile(groupName);
 
@@ -232,15 +433,17 @@ public class RifidiEdgeHelper implements Serializable {
 
 	/**
 	 * Get the application properties file
-	 * @param groupName group name
-	 * @param appName application name
+	 * 
+	 * @param groupName
+	 *            group name
+	 * @param appName
+	 *            application name
 	 * @return the application properties file
-	 * @throws PropertiesFileNotFoundException if properties file for application 
-	 * does not exist  
+	 * @throws PropertiesFileNotFoundException
+	 *             if properties file for application does not exist
 	 */
 	private static byte[] getApplicationPropertiesFile(String groupName,
-			final String appName)
-			throws PropertiesFileNotFoundException {
+			final String appName) throws PropertiesFileNotFoundException {
 
 		// the path of the directory to read files from
 		String dataPath = getDataDirPath(groupName, null);
@@ -261,49 +464,54 @@ public class RifidiEdgeHelper implements Serializable {
 
 		// if we did not read in any files
 		if (dataFiles == null || dataFiles.length < 1) {
-			throw new PropertiesFileNotFoundException(groupName,
-					appName);
+			throw new PropertiesFileNotFoundException(groupName, appName);
 		}
 
 		// Assume there is only one application property file
 		File f = dataFiles[0];
 
 		FileInputStream fileStream = null;
+		FileChannel channel = null;
+		MappedByteBuffer bb = null;
 
 		try {
 			// read in the file
 			fileStream = new FileInputStream(f);
-			FileChannel channel = fileStream.getChannel();
-			MappedByteBuffer bb = channel.map(MapMode.READ_ONLY, 0,
-					channel.size());
+			channel = fileStream.getChannel();
+			bb = channel.map(MapMode.READ_ONLY, 0, channel.size());
 			byte[] bytes = new byte[(int) channel.size()];
 			bb.get(bytes);
 
 			return bytes;
 		} catch (FileNotFoundException e) {
-			throw new PropertiesFileNotFoundException(groupName,
-					appName);
+			throw new PropertiesFileNotFoundException(groupName, appName);
 		} catch (IOException e) {
-			throw new PropertiesFileNotFoundException(groupName,
-					appName);
+			throw new PropertiesFileNotFoundException(groupName, appName);
 		} finally {
 			try {
 				if (fileStream != null) {
 					fileStream.close();
+				}
+				if (channel != null) {
+					channel.close();
+				}
+				if (bb != null) {
+					bb.clear();
 				}
 			} catch (IOException e) {
 
 			}
 		}
 	}
-	
-	
+
 	/**
-	 * Get application group properties file 
-	 * @param groupName the group name of application
+	 * Get application group properties file
+	 * 
+	 * @param groupName
+	 *            the group name of application
 	 * @return the application group properties file
-	 * @throws PropertiesFileNotFoundException if properties file for application 
-	 * group does not exist 
+	 * @throws PropertiesFileNotFoundException
+	 *             if properties file for application group does not exist
 	 */
 	private static byte[] getGroupPropertiesFile(final String groupName)
 			throws PropertiesFileNotFoundException {
@@ -360,18 +568,22 @@ public class RifidiEdgeHelper implements Serializable {
 		}
 
 	}
-	
+
 	/**
 	 * Get the readzone properties file
-	 * @param groupName the group name of application
-	 * @param appName the application name
-	 * @param readZoneName the readzone name
+	 * 
+	 * @param groupName
+	 *            the group name of application
+	 * @param appName
+	 *            the application name
+	 * @param readZoneName
+	 *            the readzone name
 	 * @return the readzone properties file
-	 * @throws PropertiesFileNotFoundException if properties file for readzone 
-	 * does not exist
+	 * @throws PropertiesFileNotFoundException
+	 *             if properties file for readzone does not exist
 	 */
-	private static byte[] getReadZonePropertiesFile(String groupName, String appName,
-			final String readZoneName)
+	private static byte[] getReadZonePropertiesFile(String groupName,
+			String appName, final String readZoneName)
 			throws PropertiesFileNotFoundException {
 
 		// the path of the directory to read files from
@@ -387,14 +599,15 @@ public class RifidiEdgeHelper implements Serializable {
 				if (arg1.endsWith("~")) {
 					return false;
 				}
-				return arg1.startsWith(READZONE_PREFIX_FILE_NAME + "-" + readZoneName);
+				return arg1.startsWith(READZONE_FILE_NAME_PREFIX + "-"
+						+ readZoneName);
 			}
 		});
 
 		// if we did not read in any files
 		if (dataFiles == null || dataFiles.length < 1) {
-			throw new PropertiesFileNotFoundException(groupName,
-					appName, readZoneName);
+			throw new PropertiesFileNotFoundException(groupName, appName,
+					readZoneName);
 		}
 
 		// Assume there is only one readzone property file
@@ -413,11 +626,11 @@ public class RifidiEdgeHelper implements Serializable {
 
 			return bytes;
 		} catch (FileNotFoundException e) {
-			throw new PropertiesFileNotFoundException(groupName,
-					appName, readZoneName);
+			throw new PropertiesFileNotFoundException(groupName, appName,
+					readZoneName);
 		} catch (IOException e) {
-			throw new PropertiesFileNotFoundException(groupName,
-					appName, readZoneName);
+			throw new PropertiesFileNotFoundException(groupName, appName,
+					readZoneName);
 		} finally {
 			try {
 				if (fileStream != null) {
@@ -428,8 +641,6 @@ public class RifidiEdgeHelper implements Serializable {
 			}
 		}
 	}
-	
-
 
 	/**
 	 * This method returns the path of the supplied directory name relative to
