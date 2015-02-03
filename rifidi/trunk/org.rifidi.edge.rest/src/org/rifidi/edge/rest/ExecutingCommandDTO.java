c1f11aeb49a24a64de0c2b6ec8d9db0742f92335
mattrifidi <mattrifidi@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2015-02-03 04:27:54 +0000

    Added the executed commands to the readerstatus command.

---
+++ rifidi/trunk/org.rifidi.edge.rest/src/org/rifidi/edge/rest/ExecutingCommandDTO.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package org.rifidi.edge.rest;
+
+import java.io.Serializable;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @author matt
+ *
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "executingcommand")
+public class ExecutingCommandDTO implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1759454640833382091L;
+
+	@XmlElement(name = "commandID")
+	private String commandID;
+	
+	@XmlElement(name = "interval")
+	private Long interval;
+
+	public String getCommandID() {
+		return commandID;
+	}
+
+	public void setCommandID(String commandID) {
+		this.commandID = commandID;
+	}
+
+	public Long getInterval() {
+		return interval;
+	}
+
+	public void setInterval(Long interval) {
+		this.interval = interval;
+	}
+}
