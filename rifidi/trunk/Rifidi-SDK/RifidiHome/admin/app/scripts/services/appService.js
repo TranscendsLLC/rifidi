c0cf8f71145436147396b698aa183c769426e050
manueltobon1 <manueltobon1@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2015-04-08 22:20:53 +0000

    Add restart app when group properties modified

---
+++ rifidi/trunk/Rifidi-SDK/RifidiHome/admin/app/scripts/services/appService.js
@@ -0,0 +1,103 @@
+/**
+ * Created by Alejandro on 07/04/2015.
+ */
+
+app.service('AppService', function($http, CommonService){
+
+    //Method that calls the API to get the applications
+    this.callAppListService = function(host){
+
+        console.log('callAppListService');
+        var serviceUrl = host + '/apps';
+        return $http({ method: 'GET', url: serviceUrl });
+
+    };
+
+    //Method that calls the API to stop application
+    this.callStopAppService = function(host, appId){
+
+        console.log('callStopAppService');
+        console.log('appId');
+        console.log(appId);
+        var serviceUrl = host + '/stopapp/' + appId;
+        return $http({ method: 'GET', url: serviceUrl });
+
+    };
+
+    //Method that calls the API to start application
+    this.callStartAppService = function(host, appId){
+
+        console.log('callStartAppService');
+        console.log('appId');
+        console.log(appId);
+        var serviceUrl = host + '/startapp/' + appId;
+        return $http({ method: 'GET', url: serviceUrl });
+
+    };
+
+    // Method that takes the xml response from server and returns the applications
+    // belonging to specific group
+
+    this.getAppsFromReceivedData = function(data, groupName){
+
+        var xmlData = CommonService.getXmlObjectFromXmlServerData(data);
+
+        //get the xml response and extract the values to construct the app list where group matches
+        var appsXmlVector = xmlData.getElementsByTagName("app");
+
+        var appsToReturn = [];
+
+        for (var index = 0; index < appsXmlVector.length; index++) {
+
+            var id = appsXmlVector[index].getElementsByTagName("id")[0].childNodes[0].nodeValue;
+            var number = appsXmlVector[index].getElementsByTagName("number")[0].childNodes[0].nodeValue;
+            var status = appsXmlVector[index].getElementsByTagName("status")[0].childNodes[0].nodeValue;
+
+            //As id comes in the form 'AppGroup:AppName' we need to split it into two different variables
+
+            var localGroupName = id.split(":")[0];
+            var appName = id.split(":")[1];
+
+            if (localGroupName == groupName) {
+
+                //Add the application to app list
+                var appElement = {
+                    "number": number,
+                    "status": status,
+                    "groupName": groupName,
+                    "appName": appName
+                };
+
+                appsToReturn.push(appElement);
+
+            }
+        }
+
+        return appsToReturn;
+
+    };
+
+    // Method that extracts from url used to stop app, the app id parameter
+    // It is expected the url has the form: "http://localhost:8111/stopapp/{appNumber}"
+
+    this.extractAppIdFromStopAppUrl = function(url){
+
+        //console.log('extractAppIdFromStopAppUrl');
+        //console.log('extractAppIdFromStopAppUrl.url:');
+        //console.log(url);
+
+        //find the text 'stopapp/' and extract value after that string
+        var word = 'stopapp/';
+        var indexInicio = url.lastIndexOf(word) + word.length;
+        var appId = url.substring(indexInicio, url.length);
+
+        //console.log("extractAppIdFromStopAppUrl.appId:");
+        //console.log(appId);
+
+        return appId;
+
+    }
+
+
+
+});
