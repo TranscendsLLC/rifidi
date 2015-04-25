2c2a12c43062e4e1f1aec8a5c952d9a069ce5379
manueltobon1 <manueltobon1@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2015-04-25 06:41:52 +0000

    config auto refresh by using server properties

--- rifidi/trunk/Rifidi-SDK/RifidiHome/admin/app/scripts/services/serverService.js
+++ rifidi/trunk/Rifidi-SDK/RifidiHome/admin/app/scripts/services/serverService.js
@@ -4,11 +4,22 @@
 
 app.service('ServerService', function($http, CommonService){
 
+    var host = 'http://localhost:8111/';
+
     //Method that calls the API to get the servers
     this.callServerListService = function(){
 
         console.log('callServerListService');
-        var serviceUrl = 'http://localhost:8111/getServersFile';
+        var serviceUrl = host + 'getServersFile';
+        return $http({ method: 'GET', url: serviceUrl });
+
+    };
+
+    //Method that calls the API to get the UI properties
+    this.callUIPropertiesService = function(){
+
+        console.log('callUIPropertiesService');
+        var serviceUrl = host + 'getUIPropertiesFile';
         return $http({ method: 'GET', url: serviceUrl });
 
     };
@@ -17,7 +28,16 @@ app.service('ServerService', function($http, CommonService){
     this.callUpdateServersService = function(dataToStore){
 
         console.log('callUpdateServersService');
-        var serviceUrl = 'http://localhost:8111/updateServersFile/' + encodeURIComponent(dataToStore);
+        var serviceUrl = host + 'updateServersFile/' + encodeURIComponent(dataToStore);
+        return $http({ method: 'GET', url: serviceUrl });
+
+    };
+
+    //Method that calls the API to update the UI properties file
+    this.callUpdateUIPropertiesService = function(dataToStore){
+
+        console.log('callUpdateUIPropertiesService');
+        var serviceUrl = host + 'updateUIPropertiesFile/' + encodeURIComponent(dataToStore);
         return $http({ method: 'GET', url: serviceUrl });
 
     };
@@ -49,6 +69,20 @@ app.service('ServerService', function($http, CommonService){
         var serverTimestamp = timestampXmlVector[0].childNodes[0].nodeValue;
 
         return serverTimestamp;
+
+    };
+
+    // Method that takes the xml response from server and returns success or failure on editing servers ui properties
+    this.getSaveServersUIMessageFromReceivedData = function(data){
+
+        var xmlData = CommonService.getXmlObjectFromXmlServerData(data);
+
+        //get the xml response and extract the message
+        var xmlVector = xmlData.getElementsByTagName("message");
+
+        var message = xmlVector[0].childNodes[0].nodeValue;
+
+        return message;
 
     };
 
