4e4523167904dd9f06c73f993df5c12affcd0898
mattrifidi <mattrifidi@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2015-05-05 03:19:55 +0000

    Removed vertx from rest.

--- org.rifidi.edge.rest/src/org/rifidi/edge/rest/RestletServer.java
+++ org.rifidi.edge.rest/src/org/rifidi/edge/rest/RestletServer.java
@@ -13,13 +13,6 @@ import org.restlet.Server;
 import org.restlet.data.Parameter;
 import org.restlet.data.Protocol;
 import org.restlet.util.Series;
-import org.vertx.java.core.Handler;
-import org.vertx.java.core.Vertx;
-import org.vertx.java.core.VertxFactory;
-import org.vertx.java.core.http.HttpServerRequest;
-import org.vertx.java.core.net.NetSocket;
-import org.vertx.java.core.streams.Pump;
-import org.vertx.java.platform.Verticle;
 
 /**
  * 
@@ -102,98 +95,10 @@ public class RestletServer {
 				sslComponent.start();
 
 			}
-			
-			//Server
-			
-			//Vertx vertx = VertxFactory.newVertx();
-			
-			
-			
-			/*
-			 * // Add vert.x server Vertx vertx = VertxFactory.newVertx();
-			 * 
-			 * // Create an echo server
-			 * vertx.createNetServer().connectHandler(new Handler<NetSocket>() {
-			 * public void handle(final NetSocket socket) {
-			 * Pump.createPump(socket, socket).start(); } }).listen(1234);
-			 * 
-			 * logger.info("Created Vertx server on port 1234");
-			 * 
-			 * // Prevent the JVM from exiting System.in.read();
-			 */
-
-			
-			
-			/*
-			vertx.createHttpServer()
-					.requestHandler(new Handler<HttpServerRequest>() {
-						public void handle(HttpServerRequest req) {
-							System.out.println("Got request: " + req.uri());
-							//System.out.println("Headers are: ");
-							// for (Map.Entry<String, String> entry :
-							// req.headers()) {
-							// System.out.println(entry.getKey() + ":" +
-							// entry.getValue());
-							// }
-							req.response()
-									.headers()
-									.set("Content-Type",
-											"text/html; charset=UTF-8");
-							req.response()
-									.end("<html><body><h1>Hello from vert.x!</h1></body></html>");
-						}
-					}).listen(7852);
-			*/
-			
-			
-			
-			/*
-			// Create an echo server
-		    vertx.createNetServer().connectHandler(new Handler<NetSocket>() {
-		      public void handle(final NetSocket socket) {
-		        Pump.createPump(socket, socket).start();
-		      }
-		    }).listen(1234);
-
-		    // Prevent the JVM from exiting
-		    System.in.read();
-			*/
-			
-			
-			
-			
-			/*
-			vertx.createHttpServer().requestHandler(new Handler<HttpServerRequest>() {
-				public void handle(HttpServerRequest req) {
-					
-				}
-			}).listen(7852);
-			*/
-			
-			
-			//RestletServer.Serverx serv = new Serverx();
-			//serv.start();
 
 		} catch (Exception e) {
 			// TODO Handle this
 			e.printStackTrace();
 		}
 	}
-	
-	/*
-	 class Serverx extends Verticle {
-		public void start() {
-	          vertx.createHttpServer().requestHandler(new Handler<HttpServerRequest>() {
-	              public void handle(HttpServerRequest req) {
-	                  //String file = req.path().equals("/") ? "index.html" : req.path();
-	                  //req.response().sendFile("webroot/" + file);
-	            	  req.response().headers().set("Content-Type", "text/html; charset=UTF-8"); 
-	            	  req.response().end("<html><body><h1>Hello from vert.x!</h1></body></html>"); 
-
-	              }
-	          }).listen(1234);
-	      }
-
-		}
-	*/
 }
