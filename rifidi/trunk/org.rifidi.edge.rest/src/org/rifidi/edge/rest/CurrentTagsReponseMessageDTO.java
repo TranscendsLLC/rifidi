942275a61370fdc84dc6de05edc12d839e207a05
mattrifidi <mattrifidi@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2015-11-19 06:35:31 +0000

    Adding currenttags to rest

---
+++ rifidi/trunk/org.rifidi.edge.rest/src/org/rifidi/edge/rest/CurrentTagsReponseMessageDTO.java
@@ -0,0 +1,46 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Transcends, LLC.
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU 
+ * General Public License as published by the Free Software Foundation; either version 2 of the 
+ * License, or (at your option) any later version. This program is distributed in the hope that it will 
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You 
+ * should have received a copy of the GNU General Public License along with this program; if not, 
+ * write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, 
+ * USA. 
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ *******************************************************************************/
+package org.rifidi.edge.rest;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @author Matthew Dean - matt@transcends.co
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "response")
+public class CurrentTagsReponseMessageDTO implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5864386747857666489L;
+	
+	@XmlElementWrapper(required = true, name="currenttags")
+	@XmlElement(name = "tag")
+	private List<String> tags;
+
+	public List<String> getTags() {
+		return tags;
+	}
+
+	public void setTags(List<String> tags) {
+		this.tags = tags;
+	}
+}
